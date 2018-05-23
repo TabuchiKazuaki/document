11a13d4b2ecea2900e1bd18b68baebbef2214b24
kazua <tabuchi_kazuaki@abema.tv>
2018-05-23 16:11:18 +0900

    no message

--- rank/rank_thr.js
+++ rank/rank_thr.js
@@ -13,4 +13,21 @@ $(document).ready(function() {
           return false;
      });
 });
+$(function(){
+    var $setElm = $('.rank_area');
+    var cutFigure = '25'; // カットする文字数
+    var afterTxt = ' …'; // 文字カット後に表示するテキスト
+
+    $setElm.each(function(){
+        var textLength = $(this).text().length;
+        var textTrim = $(this).text().substr(0,(cutFigure))
+
+        if(cutFigure < textLength) {
+            $(this).html(textTrim + afterTxt).css({visibility:'visible'});
+        } else if(cutFigure >= textLength) {
+            $(this).css({visibility:'visible'});
+        }
+    });
+});
+
 
