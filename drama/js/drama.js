d5f257a9df8c6b9e6aebf6311427796cdae1caaf
kazua <tabuchi_kazuaki@abema.tv>
2018-06-01 10:34:21 +0900

    no message

--- drama/js/drama.js
+++ drama/js/drama.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
      //初期表示
      $(".tab_content_four").hide();//全ての.tab_contentを非表示
      $("ul.tabs_four li:first").addClass("active").show();//tabs内最初のliに.activeを追加
-     $(".tab_content04:first").show();//最初の.tab_contentを表示
+     $(".tab_content_four:first").show();//最初の.tab_contentを表示
      //タブクリック時
      $("ul.tabs_four li").click(function() {
           $("ul.tabs_four li").removeClass("active");//.activeを外す
