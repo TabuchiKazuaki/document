d9e27329a3a77b7d38b47f8e4a8abce985dd2cf6
kazua <tabuchi_kazuaki@abema.tv>
2018-06-07 16:23:07 +0900

    no message

--- drama/js/drama.js
+++ drama/js/drama.js
@@ -62,19 +62,6 @@ $(document).ready(function() {
      });
 });
 
-$(function(){
-    // #で始まるアンカーをクリックした場合に処理
-    $('.scrollTo').click(function() {
-        // スクロールの速度
-        var speed = 300; // ミリ秒
-        // アンカーの値取得
-        var href= $(this).attr("href");
-        // 移動先を取得
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        // 移動先を数値で取得
-        var position = target.offset().top;
-        // スムーススクロール
-        $('body,html').animate({scrollTop:position}, speed, 'linear');
-        return false;
-    });
-});
+$(document).ready(function() {
+    $(".content_area").tile(3)
+  });
