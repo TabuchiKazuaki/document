335bffe9fba1f06ab5366842d936aff0ac5f819b
kazua <tabuchi_kazuaki@abema.tv>
2018-07-13 13:14:26 +0900

    no message

--- six_start/js/six_start_0715.js
+++ six_start/js/six_start_0715.js
@@ -1,19 +1,19 @@
-$(function(){
-    var $setElm = $('.title');
-    var cutFigure = '36'; // カットする文字数
-    var afterTxt = ' …'; // 文字カット後に表示するテキスト
+// $(function(){
+//     var $setElm = $('.title');
+//     var cutFigure = '36'; // カットする文字数
+//     var afterTxt = ' …'; // 文字カット後に表示するテキスト
 
-    $setElm.each(function(){
-        var textLength = $(this).text().length;
-        var textTrim = $(this).text().substr(0,(cutFigure))
+//     $setElm.each(function(){
+//         var textLength = $(this).text().length;
+//         var textTrim = $(this).text().substr(0,(cutFigure))
 
-        if(cutFigure < textLength) {
-            $(this).html(textTrim + afterTxt).css({visibility:'visible'});
-        } else if(cutFigure >= textLength) {
-            $(this).css({visibility:'visible'});
-        }
-    });
-});
+//         if(cutFigure < textLength) {
+//             $(this).html(textTrim + afterTxt).css({visibility:'visible'});
+//         } else if(cutFigure >= textLength) {
+//             $(this).css({visibility:'visible'});
+//         }
+//     });
+// });
 
 
 
