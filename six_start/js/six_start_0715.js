37568f34c8f2a60a0451041f10edcf84a7f69807
kazua <tabuchi_kazuaki@abema.tv>
2018-07-18 13:50:36 +0900

    no message

--- six_start/js/six_start_0715.js
+++ six_start/js/six_start_0715.js
@@ -33,8 +33,8 @@ var ele10 = document.getElementById('s_10');
 var ele11 = document.getElementById('s_11');
 var ele12 = document.getElementById('s_12');
 
-var tt_01 = new Date("2018/06/16 18:00");
-var out01 = new Date("2018/06/16 18:55");
+var tt_01 = new Date("2018/07/16 18:00");
+var out01 = new Date("2018/07/16 18:55");
 if (now.getTime() > tt_01.getTime()){
 	ele01.innerText = '今すぐ視聴する';
 } if (now.getTime() > out01.getTime()) {
@@ -42,9 +42,9 @@ if (now.getTime() > tt_01.getTime()){
 	// ele01.style.backgroundColor = "#bbb";
 	ele01.href = "https://abema.tv/video/episode/26-45_s1_p1";
 }
-var ttb_02 = new Date("2018/06/16 23:00");
-var tt_02 = new Date("2018/06/19 00:50");
-var out02 = new Date("2018/06/20 00:50");
+var ttb_02 = new Date("2018/07/16 23:00");
+var tt_02 = new Date("2018/07/19 00:50");
+var out02 = new Date("2018/07/20 00:50");
 if (now.getTime() > ttb_02.getTime()){
 	ele02.innerText = '今すぐ視聴する';
 }
@@ -57,8 +57,8 @@ if (now.getTime() > tt_02.getTime()){
 	ele02.href = "javascript:void(0)";
 }
 
-var tt_03 = new Date("2018/06/18 17:00");
-var out03 = new Date("2018/06/19 17:55");
+var tt_03 = new Date("2018/07/18 17:00");
+var out03 = new Date("2018/07/19 17:55");
 if (now.getTime() > tt_03.getTime()){
 	ele03.innerText = '今すぐ視聴する';
 } if (now.getTime() > out03.getTime()) {
@@ -66,72 +66,72 @@ if (now.getTime() > tt_03.getTime()){
 	// ele03.style.backgroundColor = "#bbb";
 	ele03.href = "https://abema.tv/video/episode/106-1_s0_p1";
 }
-var tt_04 = new Date("2018/06/19 21:00");
-var out04 = new Date("2018/06/19 22:50");
+var tt_04 = new Date("2018/07/19 21:00");
+var out04 = new Date("2018/07/19 22:50");
 if (now.getTime() > tt_04.getTime()){
 	ele04.innerText = '今すぐ視聴する';
 } if (now.getTime() > out04.getTime()) {
 	ele04.innerText = 'ビデオを視聴する';
 	ele04.href = "https://abema.tv/video/episode/26-6_s2_p1";
 }
-// var tt_05 = new Date("2018/06/16 18:00");
-// var out05 = new Date("2018/06/16 18:55");
+// var tt_05 = new Date("2018/07/16 18:00");
+// var out05 = new Date("2018/07/16 18:55");
 // if (now.getTime() > tt_05.getTime()){
 // 	ele05.innerText = '今すぐ視聴する';
 // } if (now.getTime() > out05.getTime()) {
 // 	ele05.innerText = 'ビデオを視聴する';
 // 	ele05.href = "";
 // }
-var tt_06 = new Date("2018/06/20 01:30");
-var out06 = new Date("2018/06/20 02:30");
+var tt_06 = new Date("2018/07/20 01:30");
+var out06 = new Date("2018/07/20 02:30");
 if (now.getTime() > tt_06.getTime()){
 	ele06.innerText = '今すぐ視聴する';
 } if (now.getTime() > out06.getTime()) {
 	ele06.innerText = 'ビデオを視聴する';
 	ele06.href = "https://abema.tv/video/episode/248-2_s0_p1";
 }
-// var tt_07 = new Date("2018/06/16 18:00");
-// var out07 = new Date("2018/06/16 18:55");
+// var tt_07 = new Date("2018/07/16 18:00");
+// var out07 = new Date("2018/07/16 18:55");
 // if (now.getTime() > tt_07.getTime()){
 // 	ele07.innerText = '今すぐ視聴する';
 // } if (now.getTime() > out07.getTime()) {
 // 	ele07.innerText = 'ビデオを視聴する';
 // 	ele07.href = "";
 // }
-var tt_08 = new Date("2018/06/21 22:00");
-var out08 = new Date("2018/06/21 23:25");
+var tt_08 = new Date("2018/07/21 22:00");
+var out08 = new Date("2018/07/21 23:25");
 if (now.getTime() > tt_08.getTime()){
 	ele08.innerText = '今すぐ視聴する';
 } if (now.getTime() > out08.getTime()) {
 	ele08.innerText = 'ビデオを視聴する';
 	ele08.href = "https://abema.tv/video/episode/25-92_s1_p1";
 }
-var tt_09 = new Date("2018/06/23 19:55");
-var out09 = new Date("2018/06/23 20:50");
+var tt_09 = new Date("2018/07/23 19:55");
+var out09 = new Date("2018/07/23 20:50");
 if (now.getTime() > tt_09.getTime()){
 	ele09.innerText = '今すぐ視聴する';
 } if (now.getTime() > out09.getTime()) {
 	ele09.innerText = 'ビデオを視聴する';
 	ele09.href = "https://abema.tv/video/episode/13-20xc-mkyminy_s0_p1";
 }
-var tt_10 = new Date("2018/06/28 01:30");
-var out10 = new Date("2018/06/28 02:30");
+var tt_10 = new Date("2018/07/28 01:30");
+var out10 = new Date("2018/07/28 02:30");
 if (now.getTime() > tt_10.getTime()){
 	ele10.innerText = '今すぐ視聴する';
 } if (now.getTime() > out10.getTime()) {
 	ele10.innerText = 'ビデオを視聴する';
 	ele10.href = "https://abema.tv/video/episode/248-3_s0_p1";
 }
-var tt_11 = new Date("2018/06/28 00:00");
-var out11 = new Date("2018/06/29 00:52");
+var tt_11 = new Date("2018/07/28 00:00");
+var out11 = new Date("2018/07/29 00:52");
 if (now.getTime() > tt_11.getTime()){
 	ele11.innerText = '今すぐ視聴する';
 } if (now.getTime() > out11.getTime()) {
 	ele11.innerText = 'ビデオを視聴する';
 	ele11.href = "https://abema.tv/video/episode/26-28_s1_p1";
 }
-var tt_12 = new Date("2018/06/30 17:00");
-var out12 = new Date("2018/06/30 18:00");
+var tt_12 = new Date("2018/07/30 17:00");
+var out12 = new Date("2018/07/30 18:00");
 if (now.getTime() > tt_12.getTime()){
 	ele12.innerText = '今すぐ視聴する';
 } if (now.getTime() > out12.getTime()) {
--- six_start/js/six_start_0715b.js
+++ six_start/js/six_start_0715b.js
@@ -36,16 +36,16 @@ var ele23 = document.getElementById('s_23');
 
 
 
-var tt_13 = new Date("2018/06/16 23:00");
-var out13 = new Date("2018/06/17 00:00");
+var tt_13 = new Date("2018/07/16 23:00");
+var out13 = new Date("2018/07/17 00:00");
 if (now.getTime() > tt_13.getTime()){
 	ele13.innerText = '今すぐ視聴する';
 } if (now.getTime() > out13.getTime()) {
 	ele13.innerText = 'ビデオを視聴する';
 	ele13.href = "https://abema.tv/video/episode/90-1061";
 }
-var hosi = new Date("2018/06/23 23:00");
-var hosi_out = new Date("2018/06/23 23:30");
+var hosi = new Date("2018/07/23 23:00");
+var hosi_out = new Date("2018/07/23 23:30");
 if (now.getTime() > hosi.getTime()){
 	ele13.innerText = '今すぐ視聴する';
 	ele13.href = "https://abema.tv/channels/special-plus/slots/DWLco1kiv9TudR";
@@ -55,56 +55,56 @@ if (now.getTime() > hosi.getTime()){
 	ele13.innerText = 'ビデオを視聴する';
 }
 
-var tt_14 = new Date("2018/06/28 10:00");
-var out14 = new Date("2018/06/28 10:30");
+var tt_14 = new Date("2018/07/28 10:00");
+var out14 = new Date("2018/07/28 10:30");
 if (now.getTime() > tt_14.getTime()){
 	ele14.innerText = '今すぐ視聴する';
 } if (now.getTime() > out14.getTime()) {
 	ele14.innerText = 'ビデオを視聴する';
 	ele14.href = "https://abema.tv/video/episode/90-1081";
 }
-var tt_15 = new Date("2018/06/29 22:00");
-var out15 = new Date("2018/06/29 23:00");
+var tt_15 = new Date("2018/07/29 22:00");
+var out15 = new Date("2018/07/29 23:00");
 if (now.getTime() > tt_15.getTime()){
 	ele15.innerText = '今すぐ視聴する';
 } if (now.getTime() > out15.getTime()) {
 	ele15.innerText = 'ビデオを視聴する';
 	ele15.href = "https://abema.tv/video/episode/90-1053";
 }
-var tt_16 = new Date("2018/06/25 22:10");
-var out16 = new Date("2018/06/25 23:20");
+var tt_16 = new Date("2018/07/25 22:10");
+var out16 = new Date("2018/07/25 23:20");
 if (now.getTime() > tt_16.getTime()){
 	ele16.innerText = '今すぐ視聴する';
 } if (now.getTime() > out16.getTime()) {
 	ele16.innerText = 'ビデオを視聴する';
 	ele16.href = "https://abema.tv/video/episode/193-3_s1_p1";
 }
-var tt_17 = new Date("2018/06/26 23:20");
-var out17 = new Date("2018/06/27 00:35");
+var tt_17 = new Date("2018/07/26 23:20");
+var out17 = new Date("2018/07/27 00:35");
 if (now.getTime() > tt_17.getTime()){
 	ele17.innerText = '今すぐ視聴する';
 } if (now.getTime() > out17.getTime()) {
 	ele17.innerText = 'ビデオを視聴する';
 	ele17.href = "https://abema.tv/video/episode/193-35_s0_p1";
 }
-var tt_18 = new Date("2018/06/18 22:20");
-var out18 = new Date("2018/06/18 23:30");
+var tt_18 = new Date("2018/07/18 22:20");
+var out18 = new Date("2018/07/18 23:30");
 if (now.getTime() > tt_18.getTime()){
 	ele18.innerText = '今すぐ視聴する';
 } if (now.getTime() > out18.getTime()) {
 	ele18.innerText = 'ビデオを視聴する';
 	ele18.href = "https://abema.tv/video/episode/193-53_s1_p1";
 }
-var tt_19 = new Date("2018/06/19 22:20");
-var out19 = new Date("2018/06/19 23:30");
+var tt_19 = new Date("2018/07/19 22:20");
+var out19 = new Date("2018/07/19 23:30");
 if (now.getTime() > tt_19.getTime()){
 	ele19.innerText = '今すぐ視聴する';
 } if (now.getTime() > out19.getTime()) {
 	ele19.innerText = 'ビデオを視聴する';
 	ele19.href = "https://abema.tv/video/episode/193-22_s0_p1";
 }
-var tt_20 = new Date("2018/06/24 22:25");
-var out20 = new Date("2018/06/24 23:30");
+var tt_20 = new Date("2018/07/24 22:25");
+var out20 = new Date("2018/07/24 23:30");
 if (now.getTime() > tt_20.getTime()){
 	ele20.innerText = '今すぐ視聴する';
 } if (now.getTime() > out20.getTime()) {
@@ -112,8 +112,8 @@ if (now.getTime() > tt_20.getTime()){
 	ele20.href = "https://abema.tv/video/episode/188-22_s1_p1";
 }
 
-var tt_21 = new Date("2018/06/19 22:00");
-var out21 = new Date("2018/06/19 23:00");
+var tt_21 = new Date("2018/07/19 22:00");
+var out21 = new Date("2018/07/19 23:00");
 if (now.getTime() > tt_21.getTime()){
 	ele21.innerText = '今すぐ視聴する';
 } if (now.getTime() > out21.getTime()) {
@@ -121,8 +121,8 @@ if (now.getTime() > tt_21.getTime()){
 	ele21.href = "https://abema.tv/video/title/50-4";
 }
 
-var tt_22 = new Date("2018/06/24 22:00");
-var out22 = new Date("2018/06/24 23:00");
+var tt_22 = new Date("2018/07/24 22:00");
+var out22 = new Date("2018/07/24 23:00");
 if (now.getTime() > tt_22.getTime()){
 	ele22.innerText = '今すぐ視聴する';
 } if (now.getTime() > out22.getTime()) {
@@ -130,8 +130,8 @@ if (now.getTime() > tt_22.getTime()){
 	ele22.href = "https://abema.tv/video/title/50-40";
 }
 
-var tt_23 = new Date("2018/06/30 23:00");
-var out23 = new Date("2018/06/30 23:30");
+var tt_23 = new Date("2018/07/30 23:00");
+var out23 = new Date("2018/07/30 23:30");
 if (now.getTime() > tt_23.getTime()){
 	ele23.innerText = '今すぐ視聴する';
 } if (now.getTime() > out23.getTime()) {
@@ -139,8 +139,8 @@ if (now.getTime() > tt_23.getTime()){
 	ele23.href = "https://abema.tv/video/title/134-24";
 }
 
-// var tt_24 = new Date("2018/06/16 18:00");
-// var out24 = new Date("2018/06/16 18:55");
+// var tt_24 = new Date("2018/07/16 18:00");
+// var out24 = new Date("2018/07/16 18:55");
 // if (now.getTime() > tt_24.getTime()){
 // 	ele24.innerText = '今すぐ視聴する';
 // } if (now.getTime() > out24.getTime()) {
@@ -148,8 +148,8 @@ if (now.getTime() > tt_23.getTime()){
 // 	ele24.href = "";
 // }
 
-// var tt_25 = new Date("2018/06/16 18:00");
-// var out25 = new Date("2018/06/16 18:55");
+// var tt_25 = new Date("2018/07/16 18:00");
+// var out25 = new Date("2018/07/16 18:55");
 // if (now.getTime() > tt_25.getTime()){
 // 	ele25.innerText = '今すぐ視聴する';
 // } if (now.getTime() > out25.getTime()) {
