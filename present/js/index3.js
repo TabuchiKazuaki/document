176dc19bf0c2a5bee7f55f93bebc29e458c667f0
kazua <tabuchi_kazuaki@abema.tv>
2018-08-09 18:33:14 +0900

    no message

--- present/js/index3.js
+++ present/js/index3.js
@@ -20,6 +20,18 @@ var tt20 = new Date("2018/09/01 00:00");
 if (now.getTime() > tt20.getTime()){
 	$(ch20).show();$(ct20).show();
 }
+var b020a = new Date("2018/06/18 23:59");
+if (now.getTime() > b020a.getTime()){
+	$(bt20).text('8月19日 募集開始予定');
+	$(bt20).css({'background':'#bbb'});
+	$(bt20).attr("href","javascript:void(0)");
+}
+var b020b = new Date("2018/08/18 23:59");
+if (now.getTime() > b020b.getTime()){
+	$(bt20).text('今すぐみる');
+	$(bt20).css({'background':'#50c401'});
+	$(bt20).attr("href","https://abema.tv/channels/abema-anime/slots/CmjeetjVh6dn8f");
+}
 var b020 = new Date("2018/09/17 23:59");
 if (now.getTime() > b020.getTime()){
 	$(bt20).text('キャンペーン募集は終了しました');
