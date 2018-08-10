b9d1c0e7ae21841f3d8859fc1eca2fead0feb25f
kazua <tabuchi_kazuaki@abema.tv>
2018-08-10 14:52:57 +0900

    no message

--- present/js/svt1.js
+++ present/js/svt1.js
@@ -13,17 +13,93 @@ var bt07 = document.getElementById('bt_07');
 
 
 
-// 01
-var b001 = new Date("2016/08/25 23:59");
+// 18~25
+var b001 = new Date("2018/08/25 23:59");
 if (now.getTime() > b001.getTime()){
 	$(bt01).text('キャンペーン募集は終了しました');
 	$(bt01).css({'background':'#bbb'});
 	$(bt01).attr("href","javascript:void(0)");
+	$(bt02).text('キャンペーン募集は終了しました');
+	$(bt02).css({'background':'#bbb'});
+	$(bt02).attr("href","javascript:void(0)");
+}
+// 25~9/1
+var b002a = new Date("2018/06/24 23:59");
+if (now.getTime() > b002a.getTime()){
+	$(bt03).text('8月25日 募集開始予定');
+	$(bt03).css({'background':'#bbb'});
+	$(bt03).attr("href","javascript:void(0)");
+	$(bt04).text('8月25日 募集開始予定');
+	$(bt04).css({'background':'#bbb'});
+	$(bt04).attr("href","javascript:void(0)");
+}
+var b002a = new Date("2018/08/24 23:59");
+if (now.getTime() > b002a.getTime()){
+	$(bt03).text('今すぐみる');
+	$(bt03).css({'background':'#50c401'});
+	$(bt03).attr("href","   ");
+	$(bt04).text('今すぐみる');
+	$(bt04).css({'background':'#50c401'});
+	$(bt04).attr("href","   ");
+}
+var b002 = new Date("2018/09/01 23:59");
+if (now.getTime() > b002.getTime()){
+	$(bt03).text('キャンペーン募集は終了しました');
+	$(bt03).css({'background':'#bbb'});
+	$(bt03).attr("href","javascript:void(0)");
+	$(bt04).text('キャンペーン募集は終了しました');
+	$(bt04).css({'background':'#bbb'});
+	$(bt04).attr("href","javascript:void(0)");
 }
 
 
+// 9/1 ~9/8
+var b003a = new Date("2018/06/31 23:59");
+if (now.getTime() > b003a.getTime()){
+	$(bt05).text('9月1日 募集開始予定');
+	$(bt05).css({'background':'#bbb'});
+	$(bt05).attr("href","javascript:void(0)");
+	$(bt06).text('9月1日 募集開始予定');
+	$(bt06).css({'background':'#bbb'});
+	$(bt06).attr("href","javascript:void(0)");
+}
+var b003b = new Date("2018/08/31 23:59");
+if (now.getTime() > b003b.getTime()){
+	$(bt05).text('今すぐみる');
+	$(bt05).css({'background':'#50c401'});
+	$(bt05).attr("href","   ");
+	$(bt06).text('今すぐみる');
+	$(bt06).css({'background':'#50c401'});
+	$(bt06).attr("href","   ");
+}
+var b003 = new Date("2018/09/01 23:59");
+if (now.getTime() > b003.getTime()){
+	$(bt05).text('キャンペーン募集は終了しました');
+	$(bt05).css({'background':'#bbb'});
+	$(bt05).attr("href","javascript:void(0)");
+	$(bt06).text('キャンペーン募集は終了しました');
+	$(bt06).css({'background':'#bbb'});
+	$(bt06).attr("href","javascript:void(0)");
+}
 
-
-
+// 9/8 ~9/15
+var b004a = new Date("2018/06/07 23:59");
+if (now.getTime() > b004a.getTime()){
+	$(bt07).text('9月8日 募集開始予定');
+	$(bt07).css({'background':'#bbb'});
+	$(bt07).attr("href","javascript:void(0)");
+}
+var b004b = new Date("2018/09/07 23:59");
+if (now.getTime() > b004b.getTime()){
+	$(bt07).text('今すぐみる');
+	$(bt07).css({'background':'#50c401'});
+	$(bt07).attr("href","   ");
+}
+var b004 = new Date("2018/09/15 23:59");
+if (now.getTime() > b004.getTime()){
+	$(bt07).text('キャンペーン募集は終了しました');
+	$(bt07).css({'background':'#bbb'});
+	$(bt07).attr("href","javascript:void(0)");
+}
 
 });
--- present/js/svt2.js
+++ present/js/svt2.js
@@ -2,162 +2,103 @@
 $(document).ready(function() {
 var now = new Date();
 
-var ch10 = document.getElementById('dh_10');
-var ct10 = document.getElementById('dt_10');
+var bt08 = document.getElementById('bt_08');
+var bt09 = document.getElementById('bt_09');
 var bt10 = document.getElementById('bt_10');
-
-var ch11 = document.getElementById('dh_11');
-var ct11 = document.getElementById('dt_11');
 var bt11 = document.getElementById('bt_11');
-
-var ch12 = document.getElementById('dh_12');
-var ct12 = document.getElementById('dt_12');
 var bt12 = document.getElementById('bt_12');
-
-var ch13 = document.getElementById('dh_13');
-var ct13 = document.getElementById('dt_13');
 var bt13 = document.getElementById('bt_13');
 
-var ch14 = document.getElementById('dh_14');
-var ct14 = document.getElementById('dt_14');
-var bt14 = document.getElementById('bt_14');
-
-var ch15 = document.getElementById('dh_15');
-var ct15 = document.getElementById('dt_15');
-var bt15 = document.getElementById('bt_15');
-
-var ch16 = document.getElementById('dh_16');
-var ct16 = document.getElementById('dt_16');
-var bt16 = document.getElementById('bt_16');
-
-var ch17 = document.getElementById('dh_17');
-var ct17 = document.getElementById('dt_17');
-var bt17 = document.getElementById('bt_17');
-
-var ch18 = document.getElementById('dh_18');
-var ct18 = document.getElementById('dt_18');
-var bt18 = document.getElementById('bt_18');
 
 
-// 10
-var tt10 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt10.getTime()){
-	$(ch10).show();$(ct10).show();
+// 08
+// 9/8 ~9/15
+var b004a = new Date("2018/06/07 23:59");
+if (now.getTime() > b004a.getTime()){
+	$(bt08).text('9月8日 募集開始予定');
+	$(bt08).css({'background':'#bbb'});
+	$(bt08).attr("href","javascript:void(0)");
 }
-var b010a = new Date("2018/06/18 23:59");
-if (now.getTime() > b010a.getTime()){
-	$(bt10).text('8月19日 募集開始予定');
+var b004b = new Date("2018/09/07 23:59");
+if (now.getTime() > b004b.getTime()){
+	$(bt08).text('今すぐみる');
+	$(bt08).css({'background':'#50c401'});
+	$(bt08).attr("href","   ");
+}
+var b004 = new Date("2018/09/15 23:59");
+if (now.getTime() > b004.getTime()){
+	$(bt08).text('キャンペーン募集は終了しました');
+	$(bt08).css({'background':'#bbb'});
+	$(bt08).attr("href","javascript:void(0)");
+}
+
+// 09 10
+// 9/15 ~9/22
+var b005a = new Date("2018/06/07 23:59");
+if (now.getTime() > b005a.getTime()){
+	$(bt09).text('9月15日 募集開始予定');
+	$(bt09).css({'background':'#bbb'});
+	$(bt09).attr("href","javascript:void(0)");
+	$(bt10).text('9月15日 募集開始予定');
 	$(bt10).css({'background':'#bbb'});
 	$(bt10).attr("href","javascript:void(0)");
 }
-var b010b = new Date("2018/08/21 23:59");
-if (now.getTime() > b010b.getTime()){
+var b005b = new Date("2018/09/14 23:59");
+if (now.getTime() > b005b.getTime()){
+	$(bt09).text('今すぐみる');
+	$(bt09).css({'background':'#50c401'});
+	$(bt09).attr("href","   ");
 	$(bt10).text('今すぐみる');
 	$(bt10).css({'background':'#50c401'});
-	$(bt10).attr("href","https://abema.tv/video/title/26-53");
+	$(bt10).attr("href","   ");
 }
-var b010 = new Date("2018/09/30 23:59");
-if (now.getTime() > b010.getTime()){
+var b005 = new Date("2018/09/22 23:59");
+if (now.getTime() > b005.getTime()){
+	$(bt09).text('キャンペーン募集は終了しました');
+	$(bt09).css({'background':'#bbb'});
+	$(bt09).attr("href","javascript:void(0)");
 	$(bt10).text('キャンペーン募集は終了しました');
 	$(bt10).css({'background':'#bbb'});
 	$(bt10).attr("href","javascript:void(0)");
 }
 
-// 11
-var tt11 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt11.getTime()){
-	$(ch11).show();$(ct11).show();
+
+// 11 12
+// 9/22 ~9/30
+var b006a = new Date("2018/06/07 23:59");
+if (now.getTime() > b006a.getTime()){
+	$(bt11).text('9月22日 募集開始予定');
+	$(bt11).css({'background':'#bbb'});
+	$(bt11).attr("href","javascript:void(0)");
+	$(bt12).text('9月22日 募集開始予定');
+	$(bt12).css({'background':'#bbb'});
+	$(bt12).attr("href","javascript:void(0)");
+	$(bt13).text('9月22日 募集開始予定');
+	$(bt13).css({'background':'#bbb'});
+	$(bt13).attr("href","javascript:void(0)");
 }
-var b011 = new Date("2018/09/30 23:59");
-if (now.getTime() > b011.getTime()){
+var b006b = new Date("2018/09/21 23:59");
+if (now.getTime() > b006b.getTime()){
+	$(bt11).text('今すぐみる');
+	$(bt11).css({'background':'#50c401'});
+	$(bt11).attr("href","   ");
+	$(bt12).text('今すぐみる');
+	$(bt12).css({'background':'#50c401'});
+	$(bt12).attr("href","   ");
+	$(bt13).text('今すぐみる');
+	$(bt13).css({'background':'#50c401'});
+	$(bt13).attr("href","   ");
+}
+var b006 = new Date("2018/09/22 23:59");
+if (now.getTime() > b006.getTime()){
 	$(bt11).text('キャンペーン募集は終了しました');
 	$(bt11).css({'background':'#bbb'});
 	$(bt11).attr("href","javascript:void(0)");
-}
-
-// 12
-var tt12 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt12.getTime()){
-	$(ch12).show();$(ct12).show();
-}
-var b012 = new Date("2018/09/30 23:59");
-if (now.getTime() > b012.getTime()){
 	$(bt12).text('キャンペーン募集は終了しました');
 	$(bt12).css({'background':'#bbb'});
 	$(bt12).attr("href","javascript:void(0)");
-}
-// 13
-var tt13 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt13.getTime()){
-	$(ch13).show();$(ct13).show();
-	$(bt12).attr("href","javascript:void(0)");
-}
-var b013 = new Date("2018/09/19 23:59");
-if (now.getTime() > b013.getTime()){
 	$(bt13).text('キャンペーン募集は終了しました');
 	$(bt13).css({'background':'#bbb'});
 	$(bt13).attr("href","javascript:void(0)");
 }
-// 14
-var tt14 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt14.getTime()){
-	$(ch14).show();$(ct14).show();
-}
-var b014 = new Date("2018/09/19 23:59");
-if (now.getTime() > b014.getTime()){
-	$(bt14).text('キャンペーン募集は終了しました');
-	$(bt14).css({'background':'#bbb'});
-	$(bt14).attr("href","javascript:void(0)");
-}
-// 15
-var tt15 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt15.getTime()){
-	$(ch15).show();$(ct15).show();
-}
-var b015 = new Date("2018/09/14 23:59");
-if (now.getTime() > b015.getTime()){
-	$(bt15).text('キャンペーン募集は終了しました');
-	$(bt15).css({'background':'#bbb'});
-	$(bt15).attr("href","javascript:void(0)");
-}
-// 16
-var tt16 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt16.getTime()){
-	$(ch16).show();$(ct16).show();
-}
-var b016 = new Date("2018/09/30 23:59");
-if (now.getTime() > b016.getTime()){
-	$(bt16).text('キャンペーン募集は終了しました');
-	$(bt16).css({'background':'#bbb'});
-	$(bt16).attr("href","javascript:void(0)");
-}
-// 17
-var tt17 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt17.getTime()){
-	$(ch17).show();$(ct17).show();
-}
-var b017 = new Date("2018/09/30 23:59");
-if (now.getTime() > b017.getTime()){
-	$(bt17).text('キャンペーン募集は終了しました');
-	$(bt17).css({'background':'#bbb'});
-	$(bt17).attr("href","javascript:void(0)");
-}
-// 18
-var tt18 = new Date("2018/09/01 00:00");
-if (now.getTime() > tt18.getTime()){
-	$(ch18).show();$(ct18).show();
-}
-var b018 = new Date("2018/09/10 23:59");
-if (now.getTime() > b018.getTime()){
-	$(bt18).text('キャンペーン募集は終了しました');
-	$(bt18).css({'background':'#bbb'});
-	$(bt18).attr("href","javascript:void(0)");
-}
-
-
-
-
-
-
-
 });
