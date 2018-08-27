23b9f20540d77d07e3459e8f5aca4e03429bce99
kazua <tabuchi_kazuaki@abema.tv>
2018-08-27 12:36:42 +0900

    no message

--- present/js/svt1.js
+++ present/js/svt1.js
@@ -9,12 +9,34 @@ var bt03 = document.getElementById('bt_03');
 var bt04 = document.getElementById('bt_04');
 var bt05 = document.getElementById('bt_05');
 var bt06 = document.getElementById('bt_06');
-var bt07 = document.getElementById('bt_07');
 
 
-
-// 18~25
-var b001 = new Date("2018/08/25 23:59");
+// 30~9/8
+var b001a = new Date("2018/06/31 23:59");
+if (now.getTime() > b001a.getTime()){
+	$(bt01).text('8月30日 募集開始予定');
+	$(bt01).css({'background':'#bbb'});
+	$(bt01).attr("href","javascript:void(0)");
+	$(bt02).text('8月30日 募集開始予定');
+	$(bt02).css({'background':'#bbb'});
+	$(bt02).attr("href","javascript:void(0)");
+	$(bt03).text('8月30日 募集開始予定');
+	$(bt03).css({'background':'#bbb'});
+	$(bt03).attr("href","javascript:void(0)");
+}
+var b001b = new Date("2018/09/08 00:00");
+if (now.getTime() > b001b.getTime()){
+	$(bt01).text('今すぐみる');
+	$(bt01).css({'background':'#50c401'});
+	$(bt01).attr("href","https://abema.tv/search?q=%E3%82%A8%E3%82%B9%E3%82%AF%E3%83%97%E3%82%B9%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
+	$(bt02).text('今すぐみる');
+	$(bt02).css({'background':'#50c401'});
+	$(bt02).attr("href","https://abema.tv/search?q=%E3%82%B8%E3%83%A7%E3%83%B3%E3%83%8F%E3%83%B3%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
+	$(bt03).text('今すぐみる');
+	$(bt03).css({'background':'#50c401'});
+	$(bt03).attr("href","https://abema.tv/search?q=%E3%82%B8%E3%83%A7%E3%82%B7%E3%83%A5%E3%82%A2%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
+}
+var b001 = new Date("2018/09/08 00:00");
 if (now.getTime() > b001.getTime()){
 	$(bt01).text('キャンペーン募集は終了しました');
 	$(bt01).css({'background':'#bbb'});
@@ -22,84 +44,48 @@ if (now.getTime() > b001.getTime()){
 	$(bt02).text('キャンペーン募集は終了しました');
 	$(bt02).css({'background':'#bbb'});
 	$(bt02).attr("href","javascript:void(0)");
-}
-// 25~9/1
-var b002a = new Date("2018/06/24 23:59");
-if (now.getTime() > b002a.getTime()){
-	$(bt03).text('8月25日 募集開始予定');
-	$(bt03).css({'background':'#bbb'});
-	$(bt03).attr("href","javascript:void(0)");
-	$(bt04).text('8月25日 募集開始予定');
-	$(bt04).css({'background':'#bbb'});
-	$(bt04).attr("href","javascript:void(0)");
-}
-var b002a = new Date("2018/08/24 23:59");
-if (now.getTime() > b002a.getTime()){
-	$(bt03).text('今すぐみる');
-	$(bt03).css({'background':'#50c401'});
-	$(bt03).attr("href","   ");
-	$(bt04).text('今すぐみる');
-	$(bt04).css({'background':'#50c401'});
-	$(bt04).attr("href","   ");
-}
-var b002 = new Date("2018/09/01 23:59");
-if (now.getTime() > b002.getTime()){
 	$(bt03).text('キャンペーン募集は終了しました');
 	$(bt03).css({'background':'#bbb'});
 	$(bt03).attr("href","javascript:void(0)");
-	$(bt04).text('キャンペーン募集は終了しました');
-	$(bt04).css({'background':'#bbb'});
-	$(bt04).attr("href","javascript:void(0)");
 }
 
+// 8~14
+var b002a = new Date("2018/06/24 23:59");
+if (now.getTime() > b002a.getTime()){
 
-// 9/1 ~9/8
-var b003a = new Date("2018/06/31 23:59");
-if (now.getTime() > b003a.getTime()){
-	$(bt05).text('9月1日 募集開始予定');
+	$(bt04).text('9月8日 募集開始予定');
+	$(bt04).css({'background':'#bbb'});
+	$(bt04).attr("href","javascript:void(0)");
+	$(bt05).text('9月8日 募集開始予定');
 	$(bt05).css({'background':'#bbb'});
 	$(bt05).attr("href","javascript:void(0)");
-	$(bt06).text('9月1日 募集開始予定');
+	$(bt06).text('9月8日 募集開始予定');
 	$(bt06).css({'background':'#bbb'});
 	$(bt06).attr("href","javascript:void(0)");
 }
-var b003b = new Date("2018/08/31 23:59");
-if (now.getTime() > b003b.getTime()){
+var b002b = new Date("2018/09/08 00:00");
+if (now.getTime() > b002b.getTime()){
+	$(bt04).text('今すぐみる');
+	$(bt04).css({'background':'#50c401'});
+	$(bt04).attr("href","https://abema.tv/search?q=%E3%82%B8%E3%83%A5%E3%83%B3%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 	$(bt05).text('今すぐみる');
 	$(bt05).css({'background':'#50c401'});
-	$(bt05).attr("href","   ");
+	$(bt05).attr("href","https://abema.tv/search?q=%E3%83%9B%E3%82%B7%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 	$(bt06).text('今すぐみる');
 	$(bt06).css({'background':'#50c401'});
-	$(bt06).attr("href","   ");
+	$(bt06).attr("href","https://abema.tv/search?q=%E3%82%A6%E3%82%A9%E3%83%8C%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 }
-var b003 = new Date("2018/09/01 23:59");
-if (now.getTime() > b003.getTime()){
+var b002 = new Date("2018/09/15 00:00");
+if (now.getTime() > b002.getTime()){
+	$(bt04).text('キャンペーン募集は終了しました');
+	$(bt04).css({'background':'#bbb'});
+	$(bt04).attr("href","javascript:void(0)");
 	$(bt05).text('キャンペーン募集は終了しました');
 	$(bt05).css({'background':'#bbb'});
 	$(bt05).attr("href","javascript:void(0)");
 	$(bt06).text('キャンペーン募集は終了しました');
 	$(bt06).css({'background':'#bbb'});
 	$(bt06).attr("href","javascript:void(0)");
-}
-
-// 9/8 ~9/15
-var b004a = new Date("2018/06/07 23:59");
-if (now.getTime() > b004a.getTime()){
-	$(bt07).text('9月8日 募集開始予定');
-	$(bt07).css({'background':'#bbb'});
-	$(bt07).attr("href","javascript:void(0)");
-}
-var b004b = new Date("2018/09/07 23:59");
-if (now.getTime() > b004b.getTime()){
-	$(bt07).text('今すぐみる');
-	$(bt07).css({'background':'#50c401'});
-	$(bt07).attr("href","   ");
-}
-var b004 = new Date("2018/09/15 23:59");
-if (now.getTime() > b004.getTime()){
-	$(bt07).text('キャンペーン募集は終了しました');
-	$(bt07).css({'background':'#bbb'});
-	$(bt07).attr("href","javascript:void(0)");
 }
 
 });
--- present/js/svt2.js
+++ present/js/svt2.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
 var now = new Date();
 
+var bt07 = document.getElementById('bt_07');
 var bt08 = document.getElementById('bt_08');
 var bt09 = document.getElementById('bt_09');
 var bt10 = document.getElementById('bt_10');
@@ -10,63 +11,52 @@ var bt12 = document.getElementById('bt_12');
 var bt13 = document.getElementById('bt_13');
 
 
-
-// 08
-// 9/8 ~9/15
-var b004a = new Date("2018/06/07 23:59");
+// 15~21
+var b004a = new Date("2018/06/24 23:59");
 if (now.getTime() > b004a.getTime()){
-	$(bt08).text('9月8日 募集開始予定');
-	$(bt08).css({'background':'#bbb'});
-	$(bt08).attr("href","javascript:void(0)");
-}
-var b004b = new Date("2018/09/07 23:59");
-if (now.getTime() > b004b.getTime()){
-	$(bt08).text('今すぐみる');
-	$(bt08).css({'background':'#50c401'});
-	$(bt08).attr("href","   ");
-}
-var b004 = new Date("2018/09/15 23:59");
-if (now.getTime() > b004.getTime()){
-	$(bt08).text('キャンペーン募集は終了しました');
-	$(bt08).css({'background':'#bbb'});
-	$(bt08).attr("href","javascript:void(0)");
-}
 
-// 09 10
-// 9/15 ~9/22
-var b005a = new Date("2018/06/07 23:59");
-if (now.getTime() > b005a.getTime()){
+	$(bt07).text('9月15日 募集開始予定');
+	$(bt07).css({'background':'#bbb'});
+	$(bt07).attr("href","javascript:void(0)");
+	$(bt08).text('9月15日 募集開始予定');
+	$(bt08).css({'background':'#bbb'});
+	$(bt08).attr("href","javascript:void(0)");
 	$(bt09).text('9月15日 募集開始予定');
 	$(bt09).css({'background':'#bbb'});
 	$(bt09).attr("href","javascript:void(0)");
-	$(bt10).text('9月15日 募集開始予定');
-	$(bt10).css({'background':'#bbb'});
-	$(bt10).attr("href","javascript:void(0)");
 }
-var b005b = new Date("2018/09/14 23:59");
-if (now.getTime() > b005b.getTime()){
+var b004a = new Date("2018/09/15 00:00");
+if (now.getTime() > b004a.getTime()){
+	$(bt07).text('今すぐみる');
+	$(bt07).css({'background':'#50c401'});
+	$(bt07).attr("href","https://abema.tv/search?q=%E3%82%A6%E3%82%B8%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
+	$(bt08).text('今すぐみる');
+	$(bt08).css({'background':'#50c401'});
+	$(bt08).attr("href","https://abema.tv/search?q=%E3%83%87%E3%82%A3%E3%82%A8%E3%82%A4%E3%83%88%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 	$(bt09).text('今すぐみる');
 	$(bt09).css({'background':'#50c401'});
-	$(bt09).attr("href","   ");
-	$(bt10).text('今すぐみる');
-	$(bt10).css({'background':'#50c401'});
-	$(bt10).attr("href","   ");
+	$(bt09).attr("href","https://abema.tv/search?q=%E3%83%9F%E3%83%B3%E3%82%AE%E3%83%A5%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 }
-var b005 = new Date("2018/09/22 23:59");
-if (now.getTime() > b005.getTime()){
+var b004 = new Date("2018/09/22 00:00");
+if (now.getTime() > b004.getTime()){
+	$(bt07).text('キャンペーン募集は終了しました');
+	$(bt07).css({'background':'#bbb'});
+	$(bt07).attr("href","javascript:void(0)");
+	$(bt08).text('キャンペーン募集は終了しました');
+	$(bt08).css({'background':'#bbb'});
+	$(bt08).attr("href","javascript:void(0)");
 	$(bt09).text('キャンペーン募集は終了しました');
 	$(bt09).css({'background':'#bbb'});
 	$(bt09).attr("href","javascript:void(0)");
-	$(bt10).text('キャンペーン募集は終了しました');
-	$(bt10).css({'background':'#bbb'});
-	$(bt10).attr("href","javascript:void(0)");
 }
 
+// 22~30
+var b005a = new Date("2018/06/24 23:59");
+if (now.getTime() > b005a.getTime()){
 
-// 11 12
-// 9/22 ~9/30
-var b006a = new Date("2018/06/07 23:59");
-if (now.getTime() > b006a.getTime()){
+	$(bt10).text('9月22日 募集開始予定');
+	$(bt10).css({'background':'#bbb'});
+	$(bt10).attr("href","javascript:void(0)");
 	$(bt11).text('9月22日 募集開始予定');
 	$(bt11).css({'background':'#bbb'});
 	$(bt11).attr("href","javascript:void(0)");
@@ -77,20 +67,26 @@ if (now.getTime() > b006a.getTime()){
 	$(bt13).css({'background':'#bbb'});
 	$(bt13).attr("href","javascript:void(0)");
 }
-var b006b = new Date("2018/09/21 23:59");
-if (now.getTime() > b006b.getTime()){
+var b005a = new Date("2018/09/22 00:00");
+if (now.getTime() > b005a.getTime()){
+	$(bt10).text('今すぐみる');
+	$(bt10).css({'background':'#50c401'});
+	$(bt10).attr("href","https://abema.tv/search?q=%E3%83%89%E3%82%AE%E3%83%A7%E3%83%A0%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 	$(bt11).text('今すぐみる');
 	$(bt11).css({'background':'#50c401'});
-	$(bt11).attr("href","   ");
+	$(bt11).attr("href","https://abema.tv/search?q=%E3%82%B9%E3%83%B3%E3%82%B0%E3%82%A1%E3%83%B3%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 	$(bt12).text('今すぐみる');
 	$(bt12).css({'background':'#50c401'});
-	$(bt12).attr("href","   ");
+	$(bt12).attr("href","https://abema.tv/search?q=%E3%83%90%E3%83%BC%E3%83%8E%E3%83%B3%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 	$(bt13).text('今すぐみる');
 	$(bt13).css({'background':'#50c401'});
-	$(bt13).attr("href","   ");
+	$(bt13).attr("href","https://abema.tv/search?q=%E3%83%87%E3%82%A3%E3%83%8E%E3%81%AE%E3%81%8A%E7%B5%B5%E3%81%8B%E3%81%8D%E8%89%B2%E7%B4%99%E3%83%97%E3%83%AC%E3%82%BC%E3%83%B3%E3%83%88");
 }
-var b006 = new Date("2018/09/22 23:59");
-if (now.getTime() > b006.getTime()){
+var b005 = new Date("2018/10/01 00:00");
+if (now.getTime() > b005.getTime()){
+	$(bt10).text('キャンペーン募集は終了しました');
+	$(bt10).css({'background':'#bbb'});
+	$(bt10).attr("href","javascript:void(0)");
 	$(bt11).text('キャンペーン募集は終了しました');
 	$(bt11).css({'background':'#bbb'});
 	$(bt11).attr("href","javascript:void(0)");
