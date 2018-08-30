737368afa36384f764589d3ee29d24b257f56999
kazua <tabuchi_kazuaki@abema.tv>
2018-08-30 11:34:29 +0900

    no message

--- free/js/choice_next.js
+++ free/js/choice_next.js
@@ -30,6 +30,26 @@ $(document).ready(function() {
 		$(".thumb06 li a").removeClass("active");//.activeを追加する
 		$(this).addClass('active');
 	});
+	$(".thumb07 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$('.thumb07 li a').on('click',function() {
+		$(".thumb07 li a").removeClass("active");//.activeを追加する
+		$(this).addClass('active');
+	});
+	$(".thumb08 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$('.thumb08 li a').on('click',function() {
+		$(".thumb08 li a").removeClass("active");//.activeを追加する
+		$(this).addClass('active');
+	});
+	$(".thumb09 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$('.thumb09 li a').on('click',function() {
+		$(".thumb09 li a").removeClass("active");//.activeを追加する
+		$(this).addClass('active');
+	});
+	$(".thumb10 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$('.thumb10 li a').on('click',function() {
+		$(".thumb10 li a").removeClass("active");//.activeを追加する
+		$(this).addClass('active');
+	});
 });
 
 // 動画再生
