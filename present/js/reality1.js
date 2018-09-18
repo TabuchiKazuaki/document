450190cf2bbee89a28fafb541c03b50595c569a4
kazua <tabuchi_kazuaki@abema.tv>
2018-09-18 12:24:00 +0900

    no message

--- present/js/reality1.js
+++ present/js/reality1.js
@@ -9,7 +9,7 @@ var rt04 = document.getElementById('rt_04');
 var rt05 = document.getElementById('rt_05');
 var rt06 = document.getElementById('rt_06');
 var rt07 = document.getElementById('rt_07');
-var rt08 = document.getElementById('rt_07');
+var rt08 = document.getElementById('rt_08');
 
 
 // 15~30
@@ -18,24 +18,31 @@ if (now.getTime() > r001.getTime()){
 	$(rt01).text('キャンペーン募集は終了しました');
 	$(rt01).css({'background':'#bbb'});
 	$(rt01).attr("href","javascript:void(0)");
+
 	$(rt02).text('キャンペーン募集は終了しました');
 	$(rt02).css({'background':'#bbb'});
 	$(rt02).attr("href","javascript:void(0)");
+
 	$(rt03).text('キャンペーン募集は終了しました');
 	$(rt03).css({'background':'#bbb'});
 	$(rt03).attr("href","javascript:void(0)");
+
 	$(rt04).text('キャンペーン募集は終了しました');
 	$(rt04).css({'background':'#bbb'});
 	$(rt04).attr("href","javascript:void(0)");
+
 	$(rt05).text('キャンペーン募集は終了しました');
 	$(rt05).css({'background':'#bbb'});
 	$(rt05).attr("href","javascript:void(0)");
+
 	$(rt06).text('キャンペーン募集は終了しました');
 	$(rt06).css({'background':'#bbb'});
 	$(rt06).attr("href","javascript:void(0)");
+
 	$(rt07).text('キャンペーン募集は終了しました');
 	$(rt07).css({'background':'#bbb'});
 	$(rt07).attr("href","javascript:void(0)");
+
 	$(rt08).text('キャンペーン募集は終了しました');
 	$(rt08).css({'background':'#bbb'});
 	$(rt08).attr("href","javascript:void(0)");
