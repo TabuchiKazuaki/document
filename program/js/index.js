24748e252ca17d3ed79995674039bc2c04cb38ef
kazua <tabuchi_kazuaki@abema.tv>
2018-10-12 17:32:25 +0900

    no message

--- program/js/index.js
+++ program/js/index.js
@@ -1,27 +1,13 @@
 (function() {
 
     $(function() {
-        var byRow = $('body').hasClass('test-rows');
-
-        $('.items-container').each(function() {
-            $(this).children('.item').matchHeight({
-                byRow: byRow
-            });
-        });
-
         $('.item').matchHeight({
             property: 'min-height'
         });
 
-        $('.target-items').each(function() {
+        $('.items-container').each(function() {
             $(this).children('.item-0').matchHeight({
                 target: $(this).find('.item-1')
-            });
-        });
-
-        $('.btn-remove').click(function() {
-            $('.item').matchHeight({
-                remove: true
             });
         });
     });
