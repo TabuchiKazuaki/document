b717fbf08af6469bec186518f39187fc78dfe620
kazua <tabuchi_kazuaki@abema.tv>
2018-10-15 15:50:13 +0900

    no message

--- program/js/index.js
+++ program/js/index.js
@@ -1,3 +1,19 @@
+// (function() {
+
+//     $(function() {
+//         $('.item').matchHeight({
+//             property: 'min-height'
+//         });
+
+//         $('.items-container').each(function() {
+//             $(this).children('.item-0').matchHeight({
+//                 target: $(this).find('.item-1')
+//             });
+//         });
+//     });
+
+// })();
+
 
 $(window).resize(function(){
 	$('.item').matchHeight({
