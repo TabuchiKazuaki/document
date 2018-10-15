1dd9eac24bea25687800ccfd3df08c98c430fe09
kazua <tabuchi_kazuaki@abema.tv>
2018-10-15 11:43:17 +0900

    no message

--- program/js/index.js
+++ program/js/index.js
@@ -1,15 +1,13 @@
-(function() {
 
-    $(function() {
-        $('.item').matchHeight({
-            property: 'min-height'
-        });
+$(window).on('load resize', function(){
+	$('.item').matchHeight({
+		property: 'min-height'
+	});
 
-        $('.items-container').each(function() {
-            $(this).children('.item-0').matchHeight({
-                target: $(this).find('.item-1')
-            });
-        });
-    });
+	$('.items-container').each(function() {
+		$(this).children('.item-0').matchHeight({
+			target: $(this).find('.item-1')
+		});
+	});
+});
 
-})();
