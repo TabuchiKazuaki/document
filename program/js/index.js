a0247dd541a705654b0cffdc6b876eb8b26ce3f0
kazua <tabuchi_kazuaki@abema.tv>
2018-10-16 13:13:56 +0900

    no message

--- program/js/index.js
+++ program/js/index.js
@@ -1,29 +1,29 @@
-// (function() {
+(function() {
 
-//     $(function() {
-//         $('.item').matchHeight({
-//             property: 'min-height'
-//         });
+    $(window).on('load resize', function(){
+        $('.item').matchHeight({
+            property: 'min-height'
+        });
 
-//         $('.items-container').each(function() {
-//             $(this).children('.item-0').matchHeight({
-//                 target: $(this).find('.item-1')
-//             });
-//         });
-//     });
+        $('.items-container').each(function() {
+            $(this).children('.item-0').matchHeight({
+                target: $(this).find('.item-1')
+            });
+        });
+    });
 
-// })();
+})();
 
 
-$(window).resize(function(){
-	$('.item').matchHeight({
-		property: 'min-height'
-	});
+// $(window).resize(function(){
+// 	$('.item').matchHeight({
+// 		property: 'min-height'
+// 	});
 
-	$('.items-container').each(function() {
-		$(this).children('.item-0').matchHeight({
-			target: $(this).find('.item-1')
-		});
-	});
-});
+// 	$('.items-container').each(function() {
+// 		$(this).children('.item-0').matchHeight({
+// 			target: $(this).find('.item-1')
+// 		});
+// 	});
+// });
 
