b13b1b3721210736b227d27e29576fd49bcff697
kazua <tabuchi_kazuaki@abema.tv>
2018-10-16 14:32:28 +0900

    no message

--- program/js/index.js
+++ program/js/index.js
@@ -19,12 +19,12 @@ $(document).ready(function() {
 	$('.header_list').hide();
 	$(".header_item").on('click',function() {
 		$(this).next('.header_list').fadeIn();
-		// var h = $('.header_list').height();
-		// var H = $('.header_list').height('auto').height();
-		// $('.header_list').height(h);
-		// $('.header_list').stop().animate({height : H}, 200, function() {
-		// 	$(this).height('auto');
-		// });
+		var h = $('.header_list').height();
+		var H = $('.header_list').height('auto').height();
+		$('.header_list').height(h);
+		$('.header_list').stop().animate({height : H}, 200, function() {
+			$(this).height('auto');
+		});
 	});
 });
 
