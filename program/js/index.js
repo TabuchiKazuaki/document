073a65556d808dfb7e0a2b490014f30153b2ed8d
kazua <tabuchi_kazuaki@abema.tv>
2018-10-16 15:02:36 +0900

    no message

--- program/js/index.js
+++ program/js/index.js
@@ -1,34 +1,51 @@
 (function() {
 
-    $(window).on('load resize', function(){
-        $('.item').matchHeight({
-            property: 'min-height'
-        });
+	$(window).on('load resize', function(){
+		$('.item').matchHeight({
+			property: 'min-height'
+		});
 
-        $('.items-container').each(function() {
-            $(this).children('.item-0').matchHeight({
-                target: $(this).find('.item-1')
-            });
-        });
-    });
+		$('.items-container').each(function() {
+			$(this).children('.item-0').matchHeight({
+				target: $(this).find('.item-1')
+			});
+		});
+	});
 
 })();
 
 
 $(document).ready(function() {
-	$('.header_list').hide();
+	// $('.header_list').hide();
 	$(".header_item").on('click',function() {
-		$(this).next('.header_list').fadeIn();
-		var h = $('.header_list').height();
-		var H = $('.header_list').height('auto').height();
-		$('.header_list').height(h);
-		$('.header_list').stop().animate({height : H}, 200, function() {
-			$(this).height('auto');
-		});
+		// $(this).addClass('active');
+		if($(this).hasClass("active")) {
+			$(this).removeClass("active");
+			$('.header_list').stop().animate({height : 0}, 200);
+		}
+		else {
+			$(this).addClass("active");
+			$(this).next('.header_list').show();
+			var h = $('.header_list').height();
+			var H = $('.header_list').height('auto').height();
+			$('.header_list').height(h);
+			$('.header_list').stop().animate({height : H}, 200, function() {
+				$(this).height('auto');
+			});
+		}
 	});
+
 });
 
-
+$(window).on('load resize', function(){
+	var w = $(window).width();
+	var x = 768;
+	if (w <= x) {
+		('.header_list').hide();
+	 } else {
+		('.header_list').show();
+	}
+});
 
 
 // $(window).resize(function(){
