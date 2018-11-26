a2b5021f69073338677d1e6192c011372a52e950
kazua <tabuchi_kazuaki@abema.tv>
2018-11-26 11:56:26 +0900

    no message

--- present/js/evis.js
+++ present/js/evis.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
 
 	var a001 = new Date("2018/11/27 00:00");
 	if (now.getTime() > a001.getTime()){
-		$(at01).text('初回登録なら14日間¥0トライアル');
+		$(at01).text('¥0トライアルはこちら');
 	}
-	var out01 = new Date("2018/11/29 23:59");
-	if (now.getTime() > out01.getTime()) {
-		$(at01).text('初回登録なら1ヶ月間の¥0トライアル');
-	}
+	// var out01 = new Date("2018/11/29 23:59");
+	// if (now.getTime() > out01.getTime()) {
+	// 	$(at01).text('初回登録なら1ヶ月間の¥0トライアル');
+	// }
 });
