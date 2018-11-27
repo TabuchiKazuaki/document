0eddaa0442a8ca9078c2af612d820339a3d66300
kazua <tabuchi_kazuaki@abema.tv>
2018-11-27 17:11:50 +0900

    no message

--- push_past_work_cp/js/carousel.js
+++ push_past_work_cp/js/carousel.js
@@ -48,3 +48,21 @@
 
 		});
 	});
+
+
+$(function(){
+    var $setElm = $('.txt_area');
+    var cutFigure = '18'; // カットする文字数
+    var afterTxt = ' …'; // 文字カット後に表示するテキスト
+
+    $setElm.each(function(){
+        var textLength = $(this).text().length;
+        var textTrim = $(this).text().substr(0,(cutFigure))
+
+        if(cutFigure < textLength) {
+            $(this).html(textTrim + afterTxt).css({visibility:'visible'});
+        } else if(cutFigure >= textLength) {
+            $(this).css({visibility:'visible'});
+        }
+    });
+});
