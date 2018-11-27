89384086a0bf697bc0f50a922ba9bd8c92414854
kazua <tabuchi_kazuaki@abema.tv>
2018-11-27 13:10:20 +0900

    no message

---
+++ push_past_work_cp/js/carousel.js
@@ -0,0 +1,50 @@
+	$( document ).ready(function( $ ) {
+		$( '#ranking' ).sliderPro({
+			width: 530,
+			height: 800,
+			visibleSize: '100%',
+			forceSize: 'none',
+			arrows: true,
+			loop: false,
+			fadeArrows: false,
+			autoSlideSize: false,
+			slideDistance: 30,
+			aspectRatio: 1.5,
+			autoplay: false,
+			buttons:false,
+			fadeCaption: false,
+			centerImage: true,
+			thumbnailArrows: true,
+			autoHeight: true,
+			gotoSlide: function( event ) {
+				var _this = $(this).parent('.u-clr-area[data-reactid="37"]').find('iframe[data-reactid="38"]');
+				var hl = _this.height();
+				console.log(_this.height());
+				$('hl').css('height', '-1' +'px');
+			},
+
+			breakpoints: {
+				768: {
+					slideDistance: 15,
+					width: '70%',
+					height: 300
+				},
+				// 480: {
+				// 	slideDistance: 20,
+				// 	width: 280,
+				// 	height: 158
+				// },
+				// 420: {
+				// 	slideDistance: 15,
+				// 	width: 260,
+				// 	height: 126
+				// },
+				// 375: {
+				// 	slideDistance: 10,
+				// 	width: '60%',
+				// 	height: 110
+				// }
+			}
+
+		});
+	});
