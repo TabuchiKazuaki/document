a32e1b72b483a5828db740beaaf510d81321ac3a
kazua <tabuchi_kazuaki@abema.tv>
2018-12-20 20:16:36 +0900

    no message

--- new_year/js/common.js
+++ new_year/js/common.js
@@ -1,6 +1,53 @@
 $(document).ready(function() {
 	var now = new Date();
-	// 01
+
+	//総合
+	var tt01 = new Date("2018/11/10 00:00");
+	if (now.getTime() > tt01.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463803/special-1227");
+	}
+	var tt02 = new Date("2018/12/28 00:00");
+	if (now.getTime() > tt02.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463970/special-1228");
+	}
+	var tt03 = new Date("2018/12/29 00:00");
+	if (now.getTime() > tt03.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463975/special-1229");
+	}
+	var tt04 = new Date("2018/12/30 00:00");
+	if (now.getTime() > tt04.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463977/special-1230");
+	}
+	var tt05 = new Date("2018/12/31 00:00");
+	if (now.getTime() > tt05.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463979/special-1231");
+	}
+	var tt06 = new Date("2019/01/01 00:00");
+	if (now.getTime() > tt06.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463981/special-0101");
+	}
+	var tt07 = new Date("2019/01/02 00:00");
+	if (now.getTime() > tt07.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463983/special-0102");
+	}
+	var tt08 = new Date("2019/01/03 00:00");
+	if (now.getTime() > tt08.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463988/special-0103");
+	}
+	var tt09 = new Date("2019/01/04 00:00");
+	if (now.getTime() > tt09.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463992/special-0104");
+	}
+	var tt10 = new Date("2019/01/05 00:00");
+	if (now.getTime() > tt10.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463994/special-0105");
+	}
+	var tt11 = new Date("2019/01/06 00:00");
+	if (now.getTime() > tt11.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463996/special-0106");
+	}
+
+	//アニメ
 	var bf01 = new Date("2018/11/10 00:00");
 	if (now.getTime() > bf01.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463920/animation-1227");
@@ -45,4 +92,6 @@ $(document).ready(function() {
 	if (now.getTime() > bf11.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463999/animation-0106");
 	}
+
+
 });
