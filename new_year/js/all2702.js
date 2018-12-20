a331c0ecb623a93bec55841add3759df4cef9e83
kazua <tabuchi_kazuaki@abema.tv>
2018-12-20 19:54:27 +0900

    no message

--- new_year/js/all2702.js
+++ new_year/js/all2702.js
@@ -5,44 +5,44 @@ $(document).ready(function() {
 	if (now.getTime() > bf01.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463920/animation-1227");
 	}
-	var bf01 = new Date("2018/12/28 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf02 = new Date("2018/12/28 00:00");
+	if (now.getTime() > bf02.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463974/animation-1228");
 	}
-	var bf01 = new Date("2018/12/29 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf03 = new Date("2018/12/29 00:00");
+	if (now.getTime() > bf03.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463978/animation-1229");
 	}
-	var bf01 = new Date("2018/12/30 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf04 = new Date("2018/12/30 00:00");
+	if (now.getTime() > bf04.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463980/animation-1230");
 	}
-	var bf01 = new Date("2018/12/31 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf05 = new Date("2018/12/31 00:00");
+	if (now.getTime() > bf05.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463982/animation-1231");
 	}
-	var bf01 = new Date("2018/01/01 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf06 = new Date("2018/01/01 00:00");
+	if (now.getTime() > bf06.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463987/animation-0101");
 	}
-	var bf01 = new Date("2018/01/02 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf07 = new Date("2018/01/02 00:00");
+	if (now.getTime() > bf07.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463990/animation-0102");
 	}
-	var bf01 = new Date("2018/01/03 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf08 = new Date("2018/01/03 00:00");
+	if (now.getTime() > bf08.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463993/animation-0103");
 	}
-	var bf01 = new Date("2018/01/04 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf09 = new Date("2018/01/04 00:00");
+	if (now.getTime() > bf09.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463995/animation-0104");
 	}
-	var bf01 = new Date("2018/01/05 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf10 = new Date("2018/01/05 00:00");
+	if (now.getTime() > bf10.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463997/animation-0105");
 	}
-	var bf01 = new Date("2018/01/06 00:00");
-	if (now.getTime() > bf01.getTime()) {
+	var bf11 = new Date("2018/01/06 00:00");
+	if (now.getTime() > bf11.getTime()) {
 		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463999/animation-0106");
 	}
 });
