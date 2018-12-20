f610acf947f78c06fa5874dac94ae07ce96556fb
kazua <tabuchi_kazuaki@abema.tv>
2018-12-20 18:02:09 +0900

    no message

---
+++ new_year/js/all2702.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	var now = new Date();
+	// 01
+	var bf01 = new Date("2018/11/10 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_100').attr("href","https://special-lineup.abema.tv/pages/2463920/animation-1227");
+	}
+	var bf01 = new Date("2018/12/28 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463974/animation-1228");
+	}
+	var bf01 = new Date("2018/12/29 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463978/animation-1229");
+	}
+	var bf01 = new Date("2018/12/30 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463980/animation-1230");
+	}
+	var bf01 = new Date("2018/12/31 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463982/animation-1231");
+	}
+	var bf01 = new Date("2018/01/01 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463987/animation-0101");
+	}
+	var bf01 = new Date("2018/01/02 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463990/animation-0102");
+	}
+	var bf01 = new Date("2018/01/03 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463993/animation-0103");
+	}
+	var bf01 = new Date("2018/01/04 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463995/animation-0104");
+	}
+	var bf01 = new Date("2018/01/05 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463997/animation-0105");
+	}
+	var bf01 = new Date("2018/01/06 00:00");
+	if (now.getTime() > bf01.getTime()) {
+		$('#a_101').attr("href","https://special-lineup.abema.tv/pages/2463999/animation-0106");
+	}
+});
