2f5b57a94c6cc9ce18e70d44c71fe24abd77362a
kazua <tabuchi_kazuaki@abema.tv>
2019-02-06 19:51:40 +0900

    no message

--- free/js/choice_next02.js
+++ free/js/choice_next02.js
@@ -66,6 +66,43 @@ $(document).ready(function() {
 });
 
 
+// テスト01
+$(window).bind("load", function(){
+	// 01
+	if(document.URL.match(/2583752/) ) {
+		var menuHeight = $(".choice_fix").height();
+		var startPos = 0;
+		$(window).scroll(function(){
+		  var currentPos = $(this).scrollTop();
+		  if (currentPos > startPos) {
+			if($(window).scrollTop() >= 200) {
+			  // $(".choice_fix").css("bottom", "-" + menuHeight + "px");
+			}
+		  } else {
+			$(".choice_fix").css("bottom", 0 + "px");
+		  }
+		  startPos = currentPos;
+		});
+	}
+	// 02
+	if(document.URL.match(/2583633/) ) {
+		var menuHeight = $(".choice_fix").height();
+		var startPos = 0;
+		$(window).scroll(function(){
+		  var currentPos = $(this).scrollTop();
+		  if (currentPos > startPos) {
+			if($(window).scrollTop() >= 200) {
+			  // $(".choice_fix").css("bottom", "-" + menuHeight + "px");
+			}
+		  } else {
+			$(".choice_fix").css("bottom", 0 + "px");
+		  }
+		  startPos = currentPos;
+		});
+	}
+});
+
+
 // テスト02
 $(window).bind("load", function(){
 	if(document.URL.match(/2583633/) ) {
