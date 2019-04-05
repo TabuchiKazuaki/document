d5578a979d2c29dcfb8d0baf50a75d862d844acf
kazua <tabuchi_kazuaki@abema.tv>
2019-04-05 12:45:50 +0900

    no message

--- anime_lp/js/kimetu.js
+++ anime_lp/js/kimetu.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 	var tt01 = new Date("2019/04/07 00:00");
 	if (now.getTime() > tt01.getTime()) {
-		$('#t01').attr("href","https://abema.tv/video/title/26-75 ");
+		$('#t01').attr("href","https://abema.tv/video/title/26-75");
 		$('#t01').text("視聴する")
 	}
 });
