c681b9a4198d27e7097df2307514d1db7ec36ed6
kazua <tabuchi_kazuaki@abema.tv>
2019-04-25 17:27:48 +0900

    no message

--- premium/js/ga_onr.js
+++ premium/js/ga_onr.js
@@ -5,5 +5,9 @@ $(document).ready(function() {
 	if (now.getTime() > tt01.getTime()) {
 		$('#p_01').css('display','block')
 	}
+	var tt10 = new Date("2019/05/01 22:59");
+	if (now.getTime() > tt10.getTime()) {
+		$('#p_10').css('display','block')
+	}
 
 });
