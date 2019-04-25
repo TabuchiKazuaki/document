8ee4b10418fa23049bf48af6217ed87b8c293517
kazua <tabuchi_kazuaki@abema.tv>
2019-04-25 19:49:15 +0900

    no message

--- special/js/lineup31.js
+++ special/js/lineup31.js
@@ -17,20 +17,15 @@ $(document).ready(function() {
 		$('#f_03').attr("href","https://abema.tv/video/title/90-1263");
 	}
 
-	var sec04 = new Date("2019/04/29 23:30");
-	if (now.getTime() > sec04.getTime()) {
+	var change01 = new Date("2019/04/29 23:30");
+	if (now.getTime() > change01.getTime()) {
 		$('#f_04').text('ビデオを見る');
 		$('#f_04').attr("href","https://abema.tv/video/title/90-1266");
 	}
 
-	var sec05 = new Date("2019/05/01 17:00");
-	if (now.getTime() > sec05.getTime()) {
+	var change02 = new Date("2019/05/01 17:00");
+	if (now.getTime() > change02.getTime()) {
 		$('#f_05').text('ビデオを見る');
 		$('#f_05').attr("href","https://abema.tv/video/title/203-5");
-	}
-	var sec06 = new Date("2019/04/28 23:00");
-	if (now.getTime() > sec06.getTime()) {
-		$('#f_06').text('ビデオを見る');
-		$('#f_06').attr("href","https://abema.tv/video/title/90-1248");
 	}
 });
