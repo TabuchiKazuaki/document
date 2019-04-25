7f0147ae6e023f76ac745d6463ddca96fd6610e0
kazua <tabuchi_kazuaki@abema.tv>
2019-04-25 19:53:27 +0900

    no message

--- special/js/lineup31.js
+++ special/js/lineup31.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
 	var now = new Date();
 
-	var sec01 = new Date("2019/04/28 22:00");
+	var sec01 = new Date("2019/05/10 01:20");
 	if (now.getTime() > sec01.getTime()) {
-		$('#f_01').text('ビデオを見る');
-		$('#f_01').attr("href","https://abema.tv/video/title/90-493");
+		$('#r_01').text('ビデオを見る');
+		$('#r_01').attr("href","https://abema.tv/video/title/88-79");
 	}
-	var sec02 = new Date("2019/05/01 00:30");
+	var sec02 = new Date("2019/05/08 01:20");
 	if (now.getTime() > sec02.getTime()) {
-		$('#f_02').text('ビデオを見る');
-		$('#f_02').attr("href","https://abema.tv/video/title/90-980");
+		$('#r_02').text('ビデオを見る');
+		$('#r_02').attr("href","https://abema.tv/video/title/88-81");
 	}
-	var sec03 = new Date("2019/05/01 17:00");
+	var sec03 = new Date("2019/05/09 01:20");
 	if (now.getTime() > sec03.getTime()) {
-		$('#f_03').text('ビデオを見る');
-		$('#f_03').attr("href","https://abema.tv/video/title/90-1263");
+		$('#r_03').text('ビデオを見る');
+		$('#r_03').attr("href","https://abema.tv/video/title/88-80");
 	}
 
-	var change01 = new Date("2019/04/29 23:30");
-	if (now.getTime() > change01.getTime()) {
-		$('#f_04').text('ビデオを見る');
-		$('#f_04').attr("href","https://abema.tv/video/title/90-1266");
+	var sec04 = new Date("2019/05/07 01:20");
+	if (now.getTime() > sec04.getTime()) {
+		$('#r_04').text('ビデオを見る');
+		$('#r_04').attr("href","https://abema.tv/video/title/88-78");
 	}
 
-	var change02 = new Date("2019/05/01 17:00");
-	if (now.getTime() > change02.getTime()) {
-		$('#f_05').text('ビデオを見る');
-		$('#f_05').attr("href","https://abema.tv/video/title/203-5");
+	var sec05 = new Date("2019/05/12 00:00");
+	if (now.getTime() > sec05.getTime()) {
+		$('#r_05').text('ビデオを見る');
+		$('#r_05').attr("href","https://abema.tv/video/title/90-1264");
 	}
 });
