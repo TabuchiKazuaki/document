1619229d2ac3331a0a23b919eced236a7faef96f
kazua <tabuchi_kazuaki@abema.tv>
2019-04-25 13:29:51 +0900

    no message

--- premium/js/ga_onr02.js
+++ premium/js/ga_onr02.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var now = new Date();
 
-	var tt02 = new Date("2018/05/06 00:00");
+	var tt02 = new Date("2019/05/06 00:00");
 	if (now.getTime() > tt02.getTime()) {
 		$('#p_02').hide()
 	}
