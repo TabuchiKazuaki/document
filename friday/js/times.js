baeb0ec0ddba95301ed6b8aa4dd84634ae65c426
kazua <tabuchi_kazuaki@abema.tv>
2019-06-05 18:31:27 +0900

    no message

--- friday/js/times.js
+++ friday/js/times.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
 	var tt00 = new Date("2019/05/13 23:59");
 	if (now.getTime() > tt00.getTime()) {
 		$('#bag07').show();
-		$('#bag107').show();
 		$('#bag14').hide();
-		$('#bag114').hide();
 		$('#bag21').hide();
 		$('#bag28').hide();
+
+		$('#bag107').show();
+		$('#bag114').hide();
 
 		$('#bag307').show();
 		$('#bag314').hide();
@@ -17,6 +18,9 @@ $(document).ready(function() {
 
 		$('#bag407').show();
 		$('#bag414').hide();
+
+		$('#bag521').hide();
+		$('#bag528').hide();
 
 	}
 	var tt01 = new Date("2019/06/14 00:00");
