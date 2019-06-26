fc4f7c71a5c157cb8844a681ebf83db0a05f40e1
kazua <tabuchi_kazuaki@abema.tv>
2019-06-26 13:51:28 +0900

    no message

---
+++ friday/js/times04_01.js
@@ -0,0 +1,247 @@
+$(document).ready(function() {
+	var now = new Date();
+	var def = new Date("2019/05/07 00:00");
+	if (now.getTime() > def.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').hide();
+		$('#bag21').hide();
+		$('#bag28').hide();
+
+		$('#bag107').hide();
+		$('#bag114').hide();
+
+		$('#bag307').hide();
+		$('#bag314').hide();
+		$('#bag321').hide();
+		$('#bag328').hide();
+
+		$('#bag407').hide();
+		$('#bag414').hide();
+
+		$('#bag521').hide();
+		$('#bag528').hide();
+
+
+		$('#bag1628').hide();
+		$('#bag1705').hide();
+	}
+
+	var now = new Date();
+	var tt00 = new Date("2019/06/07 00:00");
+	if (now.getTime() > tt00.getTime()) {
+		$('#bag07').show();
+		$('#bag14').hide();
+		$('#bag21').hide();
+		$('#bag28').hide();
+
+		$('#bag107').show();
+		$('#bag114').hide();
+
+		$('#bag307').show();
+		$('#bag314').hide();
+		$('#bag321').hide();
+		$('#bag328').hide();
+
+		$('#bag407').show();
+		$('#bag414').hide();
+
+		$('#bag521').hide();
+		$('#bag528').hide();
+
+		$('#bag621').hide();
+		$('#bag628').hide();
+	}
+	var now = new Date();
+	var tl00 = new Date("2019/06/08 00:00");
+	if (now.getTime() > tl00.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').hide();
+		$('#bag21').hide();
+		$('#bag28').hide();
+
+		$('#bag107').hide();
+		$('#bag114').hide();
+
+		$('#bag307').hide();
+		$('#bag314').hide();
+		$('#bag321').hide();
+		$('#bag328').hide();
+
+		$('#bag407').hide();
+		$('#bag414').hide();
+
+		$('#bag521').hide();
+		$('#bag528').hide();
+
+		$('#bag621').hide();
+		$('#bag628').hide();
+	}
+
+	var now = new Date();
+	var tt01 = new Date("2019/06/14 00:00");
+	if (now.getTime() > tt01.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').show();
+		$('#bag21').hide();
+		$('#bag28').hide();
+
+		$('#bag107').hide();
+		$('#bag114').show();
+
+		$('#bag307').hide();
+		$('#bag314').show();
+		$('#bag321').hide();
+		$('#bag328').hide();
+
+		$('#bag407').hide();
+		$('#bag414').show();
+	}
+	var now = new Date();
+	var tl01 = new Date("2019/06/15 00:00");
+	if (now.getTime() > tl01.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').hide();
+		$('#bag21').hide();
+		$('#bag28').hide();
+
+		$('#bag107').hide();
+		$('#bag114').hide();
+
+		$('#bag307').hide();
+		$('#bag314').hide();
+		$('#bag321').hide();
+		$('#bag328').hide();
+
+		$('#bag407').hide();
+		$('#bag414').hide();
+
+
+		$('#new21').hide();
+		$('#new22').hide();
+		$('#new121').hide();
+
+	}
+
+	var now = new Date();
+	var tt02 = new Date("2019/06/21 00:00");
+	if (now.getTime() > tt02.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').hide();
+		$('#bag21').show();
+		$('#bag28').hide();
+
+		$('#bag107').hide();
+		$('#bag114').hide();
+
+		$('#bag307').hide();
+		$('#bag314').hide();
+		$('#bag321').show();
+		$('#bag328').hide();
+
+		$('#bag407').hide();
+		$('#bag414').hide();
+
+		$('#bag521').show();
+		$('#bag528').hide();
+
+		$('#bag621').show();
+		$('#bag628').hide();
+	}
+	var now = new Date();
+	var tl02 = new Date("2019/06/22 00:00");
+	if (now.getTime() > tl02.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').hide();
+		$('#bag21').hide();
+		$('#bag28').hide();
+
+		$('#bag107').hide();
+		$('#bag114').hide();
+
+		$('#bag307').hide();
+		$('#bag314').hide();
+		$('#bag321').hide();
+		$('#bag328').hide();
+
+		$('#bag407').hide();
+		$('#bag414').hide();
+
+		$('#bag521').hide();
+		$('#bag528').hide();
+
+		$('#bag621').hide();
+		$('#bag628').hide();
+
+		$('#new21').hide();
+		$('#new22').hide();
+		$('#new121').hide();
+
+	}
+
+	var now = new Date();
+	var tt03 = new Date("2019/06/28 00:00");
+	if (now.getTime() > tt03.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').hide();
+		$('#bag21').hide();
+		$('#bag28').show();
+
+		$('#bag307').hide();
+		$('#bag314').hide();
+		$('#bag321').hide();
+		$('#bag328').show();
+
+		$('#bag521').hide();
+		$('#bag528').show();
+
+		$('#new21').show();
+		$('#new22').show();
+		$('#new121').show();
+
+		$('#bag621').hide();
+		$('#bag628').show();
+
+		$('#bag1628').show();
+		$('#bag1705').hide();
+	}
+
+	var now = new Date();
+	var last = new Date("2019/06/29 00:00");
+	if (now.getTime() > last.getTime()) {
+		$('#bag07').hide();
+		$('#bag14').hide();
+		$('#bag21').hide();
+		$('#bag28').hide();
+
+		$('#bag307').hide();
+		$('#bag314').hide();
+		$('#bag321').hide();
+		$('#bag328').hide();
+
+		$('#bag521').hide();
+		$('#bag528').hide();
+
+		$('#new21').hide();
+		$('#new22').hide();
+		$('#new121').hide();
+
+		$('#bag621').hide();
+		$('#bag628').hide();
+
+
+		$('#bag1628').hide();
+		$('#bag1705').hide();
+	}
+
+	var now = new Date();
+	var tt75 = new Date("2019/07/05 00:00");
+	if (now.getTime() > tt75.getTime()) {
+		$('#bag1705').show();
+	}
+
+	var now = new Date();
+	var tt75last = new Date("2019/07/06 00:00");
+	if (now.getTime() > tt75last.getTime()) {
+		$('#bag1705').hide();
+	}
+});
