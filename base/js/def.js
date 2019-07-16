d53593be972b6274b6631b03d17062f1b861c6ac
kazua <tabuchi_kazuaki@abema.tv>
2019-07-16 12:27:10 +0900

    no message

--- base/js/def.js
+++ base/js/def.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
 	 //初期表示
-	 $(".tab_content01").hide();//全ての.tab_contentを非表示
-	 $("ul.thumb01 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
-	 $(".tab_content01:first").show();//最初の.tab_contentを表示
-	 //タブクリック時
-	 $("ul.thumb01 li").click(function() {
-		  $("ul.thumb01 li a").removeClass("active");//.activeを外す
-		  $(this).children('a').addClass("active");//クリックタブに.activeを追加
-		  $(".tab_content01").hide();
-		  var activeTab = $(this).find("a").attr("href");
-		  $(activeTab).fadeIn();
-		  return false;
-	 });
+	$(".tab_content01").hide();//全ての.tab_contentを非表示
+	$("ul.thumb01 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$(".tab_content01:first").show();//最初の.tab_contentを表示
+	//タブクリック時
+	$("ul.thumb01 li").click(function() {
+		$("ul.thumb01 li a").removeClass("active");//.activeを外す
+		$(this).children('a').addClass("active");//クリックタブに.activeを追加
+		$(".tab_content01").hide();
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).fadeIn();
+		return false;
+	});
 
 
-	 $(".tab_content02").hide();//全ての.tab_contentを非表示
-	 $("ul.thumb02 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
-	 $(".tab_content02:first").show();//最初の.tab_contentを表示
-	 //タブクリック時
-	 $("ul.thumb02 li").click(function() {
-		  $("ul.thumb02 li a").removeClass("active");//.activeを外す
-		  $(this).children('a').addClass("active");//クリックタブに.activeを追加
-		  $(".tab_content02").hide();
-		  var activeTab = $(this).find("a").attr("href");
-		  $(activeTab).fadeIn();
-		  return false;
-	 });
+	$(".tab_content02").hide();//全ての.tab_contentを非表示
+	$("ul.thumb02 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$(".tab_content02:first").show();//最初の.tab_contentを表示
+	//タブクリック時
+	$("ul.thumb02 li").click(function() {
+		$("ul.thumb02 li a").removeClass("active");//.activeを外す
+		$(this).children('a').addClass("active");//クリックタブに.activeを追加
+		$(".tab_content02").hide();
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).fadeIn();
+		return false;
+	});
 
 	  $(".tab_content03").hide();//全ての.tab_contentを非表示
 	 $("ul.thumb03 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
