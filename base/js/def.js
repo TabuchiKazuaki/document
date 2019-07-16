0877536e37275f46bab8b68d3fb211dccf0b8a18
kazua <tabuchi_kazuaki@abema.tv>
2019-07-16 13:48:39 +0900

    no message

--- base/js/def.js
+++ base/js/def.js
@@ -63,4 +63,16 @@ $(document).ready(function() {
 		$(activeTab).fadeIn();
 		return false;
 	});
+	$(".tab_content06").hide();//全ての.tab_contentを非表示
+	$("ul.thumb06 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$(".tab_content06:first").show();//最初の.tab_contentを表示
+	//タブクリック時
+	$("ul.thumb06 li").click(function() {
+		$("ul.thumb06 li a").removeClass("active");//.activeを外す
+		$(this).children('a').addClass("active");//クリックタブに.activeを追加
+		$(".tab_content06").hide();
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).fadeIn();
+		return false;
+	});
 });
