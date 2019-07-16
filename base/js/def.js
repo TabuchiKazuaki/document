20013c6b9d267d1ae8c6fd4c91b7847eaaf3b12c
kazua <tabuchi_kazuaki@abema.tv>
2019-07-16 12:28:02 +0900

    no message

--- base/js/def.js
+++ base/js/def.js
@@ -27,40 +27,40 @@ $(document).ready(function() {
 		return false;
 	});
 
-	  $(".tab_content03").hide();//全ての.tab_contentを非表示
-	 $("ul.thumb03 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
-	 $(".tab_content03:first").show();//最初の.tab_contentを表示
-	 //タブクリック時
-	 $("ul.thumb03 li").click(function() {
-		  $("ul.thumb03 li a").removeClass("active");//.activeを外す
-		  $(this).children('a').addClass("active");//クリックタブに.activeを追加
-		  $(".tab_content03").hide();
-		  var activeTab = $(this).find("a").attr("href");
-		  $(activeTab).fadeIn();
-		  return false;
-	 });
-	  $(".tab_content04").hide();//全ての.tab_contentを非表示
-	 $("ul.thumb04 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
-	 $(".tab_content04:first").show();//最初の.tab_contentを表示
-	 //タブクリック時
-	 $("ul.thumb04 li").click(function() {
-		  $("ul.thumb04 li a").removeClass("active");//.activeを外す
-		  $(this).children('a').addClass("active");//クリックタブに.activeを追加
-		  $(".tab_content04").hide();
-		  var activeTab = $(this).find("a").attr("href");
-		  $(activeTab).fadeIn();
-		  return false;
-	 });
-	  $(".tab_content05").hide();//全ての.tab_contentを非表示
-	 $("ul.thumb05 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
-	 $(".tab_content05:first").show();//最初の.tab_contentを表示
-	 //タブクリック時
-	 $("ul.thumb05 li").click(function() {
-		  $("ul.thumb05 li a").removeClass("active");//.activeを外す
-		  $(this).children('a').addClass("active");//クリックタブに.activeを追加
-		  $(".tab_content05").hide();
-		  var activeTab = $(this).find("a").attr("href");
-		  $(activeTab).fadeIn();
-		  return false;
-	 });
+	$(".tab_content03").hide();//全ての.tab_contentを非表示
+	$("ul.thumb03 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$(".tab_content03:first").show();//最初の.tab_contentを表示
+	//タブクリック時
+	$("ul.thumb03 li").click(function() {
+		$("ul.thumb03 li a").removeClass("active");//.activeを外す
+		$(this).children('a').addClass("active");//クリックタブに.activeを追加
+		$(".tab_content03").hide();
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).fadeIn();
+		return false;
+	});
+	$(".tab_content04").hide();//全ての.tab_contentを非表示
+	$("ul.thumb04 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$(".tab_content04:first").show();//最初の.tab_contentを表示
+	//タブクリック時
+	$("ul.thumb04 li").click(function() {
+		$("ul.thumb04 li a").removeClass("active");//.activeを外す
+		$(this).children('a').addClass("active");//クリックタブに.activeを追加
+		$(".tab_content04").hide();
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).fadeIn();
+		return false;
+	});
+	$(".tab_content05").hide();//全ての.tab_contentを非表示
+	$("ul.thumb05 li:first a").addClass("active").show();//tabs内最初のliに.activeを追加
+	$(".tab_content05:first").show();//最初の.tab_contentを表示
+	//タブクリック時
+	$("ul.thumb05 li").click(function() {
+		$("ul.thumb05 li a").removeClass("active");//.activeを外す
+		$(this).children('a').addClass("active");//クリックタブに.activeを追加
+		$(".tab_content05").hide();
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).fadeIn();
+		return false;
+	});
 });
