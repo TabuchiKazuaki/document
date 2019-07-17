458f75e2d3a4a3fea94cd7834f319f41aa053cc3
kazua <tabuchi_kazuaki@abema.tv>
2019-07-17 14:18:12 +0900

    no message

--- friday/js/times07_01.js
+++ friday/js/times07_01.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
 	var now = new Date();
 	var tl02 = new Date("2019/06/22 00:00");
 	if (now.getTime() > tl02.getTime()) {
-		$('#th_now').hide();
+		$('.th_now').hide();
 	}
 
 	var now = new Date();
 	var ts00 = new Date("2019/07/19 00:00");
 	if (now.getTime() > ts00.getTime()) {
-		$('#th_now').show();
+		$('.th_now').show();
 	}
 
 	var now = new Date();
 	var tl00 = new Date("2019/07/20 00:00");
 	if (now.getTime() > tl00.getTime()) {
-		$('#th_now').hide();
+		$('.th_now').hide();
 	}
 });
