fcb7167c349002b5a1375a93652cf104de63a4de
kazua <tabuchi_kazuaki@abema.tv>
2019-07-18 13:31:28 +0900

    no message

--- soukessan/js/anime01.js
+++ soukessan/js/anime01.js
@@ -18,6 +18,12 @@ $(document).ready(function() {
 		$('#bag0419').hide();
 		$('#bag0426').hide();
 
+		$('.bag0816').hide();
+		$('.bag0823').hide();
+
+		$('.bag0802').hide();
+		$('.bag0809').hide();
+
 	 }
 
 	 var now = new Date();
@@ -60,6 +66,9 @@ $(document).ready(function() {
 
 		$('#bag0302').show();
 		$('#bag0309').hide();
+
+		$('.bag0802').show();
+
 	}
 
 	var tt05 = new Date("2019/08/09 00:00");
@@ -72,6 +81,10 @@ $(document).ready(function() {
 
 		$('#bag0302').hide();
 		$('#bag0309').show();
+
+		$('.bag0802').hide();
+
+		$('.bag0809').show();
 	}
 
 	var tt06last = new Date("2019/08/15 23:59");
@@ -84,6 +97,28 @@ $(document).ready(function() {
 
 		$('#bag0302').hide();
 		$('#bag0309').hide();
+
+		$('.bag0809').hide();
+	}
+
+	var tt07 = new Date("2019/08/16 00:00");
+	if (now.getTime() > tt07.getTime()) {
+		$('.bag0816').show();
+	}
+
+	var tt07last = new Date("2019/08/22 23:59");
+	if (now.getTime() > tt07last.getTime()) {
+		$('.bag0816').hide();
+	}
+
+	var tt08 = new Date("2019/08/23 00:00");
+	if (now.getTime() > tt08.getTime()) {
+		$('.bag0823').show();
+	}
+
+	var tt08last = new Date("2019/08/29 23:59");
+	if (now.getTime() > tt08last.getTime()) {
+		$('.bag0823').hide();
 	}
 
 });
