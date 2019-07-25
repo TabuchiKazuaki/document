09604095d3b9e7bac85f5558921fee094c0fd38e
kazua <tabuchi_kazuaki@abema.tv>
2019-07-25 16:18:00 +0900

    no message

--- premium/js/cash100.js
+++ premium/js/cash100.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 	var tt01 = new Date("2019/05/04 15:15");
 	if (now.getTime() > tt01.getTime()) {
-		$('.now3').show();
+		$('.now3').hide();
 		$('.now2').hide();
 		$('.now1').hide();
 		$('.now0').hide();
