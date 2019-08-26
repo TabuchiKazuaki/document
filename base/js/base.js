6196884a7866c3b3d5cc6766f66b74ec1eb0f6a8
kazua <tabuchi_kazuaki@abema.tv>
2019-08-26 11:54:12 +0900

    no message

--- base/js/base.js
+++ base/js/base.js
@@ -34,18 +34,3 @@ $(function() {
 
 
 
-$(window).bind("load", function(){
-	$('.scrollTo').click(function() {
-		// スクロールの速度
-		var speed = 300; // ミリ秒
-		// アンカーの値取得
-		var href= $(this).attr("href");
-		// 移動先を取得
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		// 移動先を数値で取得
-		var position = target.offset().top;
-		// スムーススクロール
-		$('body,html').animate({scrollTop:position}, speed, 'linear');
-		return false;
-	});
-});
