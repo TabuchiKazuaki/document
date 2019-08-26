1591ae598dbdb0e3609db747849146371e3607e6
kazua <tabuchi_kazuaki@abema.tv>
2019-08-26 16:47:02 +0900

    no message

--- base/js/base.js
+++ base/js/base.js
@@ -34,3 +34,20 @@ $(function() {
 
 
 
+$(window).bind("load", function(){
+	$('.scrollTo').click(function() {
+		var speed = 300; // ミリ秒
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$('body,html').animate({scrollTop:position}, speed, 'linear');
+		return false;
+	});
+});
+$(function(){
+  $('.btnHamburger').on('click', function(){
+    $(this).toggleClass('is-active');
+    $(this).next('.module_num').fadeToggle();
+    return false;
+  });
+});
