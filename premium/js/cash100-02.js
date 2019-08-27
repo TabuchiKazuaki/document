75a35bc9193aeab7c30f428bc03c42606183832c
kazua <tabuchi_kazuaki@abema.tv>
2019-08-27 15:05:48 +0900

    no message

--- premium/js/cash100-02.js
+++ premium/js/cash100-02.js
@@ -59,27 +59,26 @@ $(document).ready(function() {
 	}
 	var tt04 = new Date("2019/08/27 00:00");
 	if (now.getTime() > tt04.getTime()) {
-		$('.now5').hide();
-		$('.now4').show();
+		$('.now5').show();
 	}
 	var tt03 = new Date("2019/08/28 00:00");
 	if (now.getTime() > tt03.getTime()) {
-		$('.now4').hide();
-		$('.now3').show();
+		$('.now5').hide();
+		$('.now4').show();
 	}
 	var tt02 = new Date("2019/08/29 00:00");
 	if (now.getTime() > tt02.getTime()) {
-		$('.now3').hide();
-		$('.now2').show();
+		$('.now4').hide();
+		$('.now3').show();
 	}
 	var tt01 = new Date("2019/08/30 00:00");
 	if (now.getTime() > tt01.getTime()) {
-		$('.now2').hide();
-		$('.now1').show();
+		$('.now3').hide();
+		$('.now2').show();
 	}
 	var tt00 = new Date("2019/08/31 00:00");
 	if (now.getTime() > tt00.getTime()) {
-		$('.now1').hide();
+		$('.now2').hide();
 		$('.now0').show();
 	}
 	var ttlast = new Date("2019/09/01 00:00");
