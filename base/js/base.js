7b3606e548a86276401c78bfe9269ec1ab53d2fc
kazua <tabuchi_kazuaki@abema.tv>
2019-08-28 15:54:22 +0900

    no message

--- base/js/base.js
+++ base/js/base.js
@@ -62,7 +62,7 @@ $(function(){
             var targetElement = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            if (scroll > targetElement - windowHeight + 200){
+            if (scroll > targetElement - windowHeight + 150){
                 $(this).css('opacity','1');
                 $(this).css('transform','translateY(0)');
             }
