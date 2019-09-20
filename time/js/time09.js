66652d9fc6b308a8aee137966e45faec1cb8bec2
kazua <tabuchi_kazuaki@abema.tv>
2019-09-20 14:11:59 +0900

    no message

---
+++ time/js/time09.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	var now = new Date();
+	// var timer01 = new Date("2019/09/24 12:00");
+	// if (now.getTime() > timer01.getTime()) {
+	// 	$('#date01').attr("href","https://abema.tv/video/title/90-493");
+	// }
+	// var timer02 = new Date("2019/09/24 12:00");
+	// if (now.getTime() > timer02.getTime()) {
+	// 	$('#date02').attr("href","https://abema.tv/video/title/90-493");
+	// }
+	// var timer03 = new Date("2019/09/25 12:00");
+	// if (now.getTime() > timer03.getTime()) {
+	// 	$('#date03').attr("href","https://abema.tv/video/title/90-493");
+	// }
+	// var timer04 = new Date("2019/09/27 12:00");
+	// if (now.getTime() > timer04.getTime()) {
+	// 	$('#date04').attr("href","https://abema.tv/video/title/90-493");
+	// }
+});
+
+
