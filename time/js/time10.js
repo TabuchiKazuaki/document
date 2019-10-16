bfae82944ea999ab9961dd51357dff626d1b21d9
kazua <tabuchi_kazuaki@abema.tv>
2019-10-16 18:47:24 +0900

    no message

--- time/js/time10.js
+++ time/js/time10.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-	var now = new Date();
-	var timer01before = new Date("2019/09/25 12:00");
-	if (now.getTime() > timer01before.getTime()) {
-		$('#date01').hide();
-		$('#date04').hide();
-	}
-	var timer01 = new Date("2019/09/25 12:00");
-	if (now.getTime() > timer01.getTime()) {
-		$('#date01').show();
-	}
-	var timer01end = new Date("2019/10/25 12:00");
-	if (now.getTime() > timer01end.getTime()) {
-		$('#date01').hide();
-	}
+	// var now = new Date();
+	// var timer01before = new Date("2019/09/25 12:00");
+	// if (now.getTime() > timer01before.getTime()) {
+	// 	$('#date01').hide();
+	// 	$('#date04').hide();
+	// }
+	// var timer01 = new Date("2019/09/25 12:00");
+	// if (now.getTime() > timer01.getTime()) {
+	// 	$('#date01').show();
+	// }
+	// var timer01end = new Date("2019/10/25 12:00");
+	// if (now.getTime() > timer01end.getTime()) {
+	// 	$('#date01').hide();
+	// }
 	// var timer02 = new Date("2019/09/24 12:00");
 	// if (now.getTime() > timer02.getTime()) {
 	// 	$('#date02').attr("href","https://abema.tv/video/title/90-1300?s=90-1300_s200");
@@ -23,14 +23,14 @@ $(document).ready(function() {
 	// 	// $('#date03').attr("href","https://abema.tv/video/title/90-493");
 	// 	$('#date03').text('今すぐみる');
 	// }
-	var timer04 = new Date("2019/09/28 01:20");
-	if (now.getTime() > timer04.getTime()) {
-		$('#date04').show();
-	}
-	var timer04end = new Date("2020/09/28 01:19");
-	if (now.getTime() > timer04end.getTime()) {
-		$('#date04').hide();
-	}
+	// var timer04 = new Date("2019/09/28 01:20");
+	// if (now.getTime() > timer04.getTime()) {
+	// 	$('#date04').show();
+	// }
+	// var timer04end = new Date("2020/09/28 01:19");
+	// if (now.getTime() > timer04end.getTime()) {
+	// 	$('#date04').hide();
+	// }
 });
 
 
