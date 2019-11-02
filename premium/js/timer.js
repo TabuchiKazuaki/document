41fd7d838fe94e361d06b63242177715eb05baf4
kazua <tabuchi_kazuaki@abema.tv>
2019-11-02 16:34:32 +0900

    no message

--- premium/js/timer.js
+++ premium/js/timer.js
@@ -1,69 +1,9 @@
 $(function() {
-	countDown();
-	countDown00();
-	countDown01();
 	countDown02();
+	countDown01();
+	countDown00();
+	countDown();
 });
-function countDown() {
-	var ts00 = new Date("2019/11/02");
-	if (now.getTime() > ts00.getTime()) {
-		var time = new Date();
-		var end = new Date("2019/11/04 23:59");
-		var left = end - time;
-		var a_day = 24 * 60 * 60 * 1000;
-
-		var d = Math.floor(left / a_day)
-		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-		var s = Math.floor((left % a_day) / 1000) % 60 % 60
-
-		$("#day").text(d);
-		$("#hour").text(h);
-		$("#min").text(m);
-		$("#sec").text(s);
-		setTimeout('countDown()', 1000);
-	}
-}
-function countDown00() {
-	var ts01 = new Date("2019/11/09");
-	if (now.getTime() > ts01.getTime()) {
-		var time = new Date();
-		var end = new Date("2019/11/10 23:59");
-		var left = end - time;
-		var a_day = 24 * 60 * 60 * 1000;
-
-		var d = Math.floor(left / a_day)
-		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-		var s = Math.floor((left % a_day) / 1000) % 60 % 60
-
-		$("#day").text(d);
-		$("#hour").text(h);
-		$("#min").text(m);
-		$("#sec").text(s);
-		setTimeout('countDown()', 1000);
-	}
-}
-function countDown01() {
-	var ts02 = new Date("2019/11/16");
-	if (now.getTime() > ts02.getTime()) {
-		var time = new Date();
-		var end = new Date("2019/11/17 23:59");
-		var left = end - time;
-		var a_day = 24 * 60 * 60 * 1000;
-
-		var d = Math.floor(left / a_day)
-		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-		var s = Math.floor((left % a_day) / 1000) % 60 % 60
-
-		$("#day").text(d);
-		$("#hour").text(h);
-		$("#min").text(m);
-		$("#sec").text(s);
-		setTimeout('countDown()', 1000);
-	}
-}
 function countDown02() {
 	var ts03 = new Date("2019/11/30");
 	if (now.getTime() > ts03.getTime()) {
@@ -84,6 +24,69 @@ function countDown02() {
 		setTimeout('countDown()', 1000);
 	}
 }
+
+function countDown01() {
+	var ts02 = new Date("2019/11/16");
+	if (now.getTime() > ts02.getTime()) {
+		var time = new Date();
+		var end = new Date("2019/11/17 23:59");
+		var left = end - time;
+		var a_day = 24 * 60 * 60 * 1000;
+
+		var d = Math.floor(left / a_day)
+		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+		var s = Math.floor((left % a_day) / 1000) % 60 % 60
+
+		$("#day").text(d);
+		$("#hour").text(h);
+		$("#min").text(m);
+		$("#sec").text(s);
+		setTimeout('countDown()', 1000);
+	}
+}
+
+function countDown00() {
+	var ts01 = new Date("2019/11/09");
+	if (now.getTime() > ts01.getTime()) {
+		var time = new Date();
+		var end = new Date("2019/11/10 23:59");
+		var left = end - time;
+		var a_day = 24 * 60 * 60 * 1000;
+
+		var d = Math.floor(left / a_day)
+		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+		var s = Math.floor((left % a_day) / 1000) % 60 % 60
+
+		$("#day").text(d);
+		$("#hour").text(h);
+		$("#min").text(m);
+		$("#sec").text(s);
+		setTimeout('countDown()', 1000);
+	}
+}
+function countDown() {
+	var ts00 = new Date("2019/11/02");
+	if (now.getTime() > ts00.getTime()) {
+		var time = new Date();
+		var end = new Date("2019/11/04 23:59");
+		var left = end - time;
+		var a_day = 24 * 60 * 60 * 1000;
+
+		var d = Math.floor(left / a_day)
+		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+		var s = Math.floor((left % a_day) / 1000) % 60 % 60
+
+		$("#day").text(d);
+		$("#hour").text(h);
+		$("#min").text(m);
+		$("#sec").text(s);
+		setTimeout('countDown()', 1000);
+	}
+}
+
 
 $(document).ready(function() {
 	var now = new Date();
