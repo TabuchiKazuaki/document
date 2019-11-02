7220596203b6f61486c3b9a33b1ceb6c8e198607
kazua <tabuchi_kazuaki@abema.tv>
2019-11-02 16:36:29 +0900

    no message

--- premium/js/timer.js
+++ premium/js/timer.js
@@ -1,14 +1,12 @@
 $(function() {
-	countDown02();
-	countDown01();
-	countDown00();
 	countDown();
 });
-function countDown02() {
-	var ts03 = new Date("2019/11/30");
-	if (now.getTime() > ts03.getTime()) {
+
+function countDown() {
+	var ts00 = new Date("2019/11/02");
+	if (now.getTime() > ts00.getTime()) {
 		var time = new Date();
-		var end = new Date("2019/11/30 23:59");
+		var end = new Date("2019/11/04 23:59");
 		var left = end - time;
 		var a_day = 24 * 60 * 60 * 1000;
 
@@ -23,30 +21,6 @@ function countDown02() {
 		$("#sec").text(s);
 		setTimeout('countDown()', 1000);
 	}
-}
-
-function countDown01() {
-	var ts02 = new Date("2019/11/16");
-	if (now.getTime() > ts02.getTime()) {
-		var time = new Date();
-		var end = new Date("2019/11/17 23:59");
-		var left = end - time;
-		var a_day = 24 * 60 * 60 * 1000;
-
-		var d = Math.floor(left / a_day)
-		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-		var s = Math.floor((left % a_day) / 1000) % 60 % 60
-
-		$("#day").text(d);
-		$("#hour").text(h);
-		$("#min").text(m);
-		$("#sec").text(s);
-		setTimeout('countDown()', 1000);
-	}
-}
-
-function countDown00() {
 	var ts01 = new Date("2019/11/09");
 	if (now.getTime() > ts01.getTime()) {
 		var time = new Date();
@@ -65,12 +39,28 @@ function countDown00() {
 		$("#sec").text(s);
 		setTimeout('countDown()', 1000);
 	}
-}
-function countDown() {
-	var ts00 = new Date("2019/11/02");
-	if (now.getTime() > ts00.getTime()) {
+	var ts02 = new Date("2019/11/16");
+	if (now.getTime() > ts02.getTime()) {
 		var time = new Date();
-		var end = new Date("2019/11/04 23:59");
+		var end = new Date("2019/11/17 23:59");
+		var left = end - time;
+		var a_day = 24 * 60 * 60 * 1000;
+
+		var d = Math.floor(left / a_day)
+		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+		var s = Math.floor((left % a_day) / 1000) % 60 % 60
+
+		$("#day").text(d);
+		$("#hour").text(h);
+		$("#min").text(m);
+		$("#sec").text(s);
+		setTimeout('countDown()', 1000);
+	}
+	var ts03 = new Date("2019/11/30");
+	if (now.getTime() > ts03.getTime()) {
+		var time = new Date();
+		var end = new Date("2019/11/30 23:59");
 		var left = end - time;
 		var a_day = 24 * 60 * 60 * 1000;
 
