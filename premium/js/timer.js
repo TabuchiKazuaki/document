fe8b58523823282553d16092aa6c591f2d6d9e4d
kazua <tabuchi_kazuaki@abema.tv>
2019-11-05 15:15:20 +0900

    no message

--- premium/js/timer.js
+++ premium/js/timer.js
@@ -157,15 +157,8 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() {
-	$("iframe").on("load", function(){
-		try {
-			$(this).height(0);
-			$(this).height(this.contentWindow.document.documentElement.scrollHeight);
-		} catch (e) {
-		}
-	});
-});
+
+
 
 
 
