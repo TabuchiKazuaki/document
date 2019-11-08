be485ab49cad9a8e8683221b7842813c60692a9e
kazua <tabuchi_kazuaki@abema.tv>
2019-11-08 12:27:07 +0900

    no message

--- premium/js/timer.js
+++ premium/js/timer.js
@@ -57,42 +57,38 @@ function countDown() {
 		$("#sec").text(s);
 		setTimeout('countDown()', 1000);
 	}
-	// var ts03 = new Date("2019/11/23");
-	// if (now.getTime() > ts03.getTime()) {
-	// 	var time = new Date();
-	// 	var end = new Date("2019/11/24 23:59");
-	// 	var left = end - time;
-	// 	var a_day = 24 * 60 * 60 * 1000;
-
-	// 	var d = Math.floor(left / a_day)
-	// 	var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-	// 	var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-	// 	var s = Math.floor((left % a_day) / 1000) % 60 % 60
-
-	// 	$("#day").text(d);
-	// 	$("#hour").text(h);
-	// 	$("#min").text(m);
-	// 	$("#sec").text(s);
-	// 	setTimeout('countDown()', 1000);
-	// }
-	// var ts04 = new Date("2019/11/30");
-	// if (now.getTime() > ts04.getTime()) {
-	// 	var time = new Date();
-	// 	var end = new Date("2019/11/30 23:59");
-	// 	var left = end - time;
-	// 	var a_day = 24 * 60 * 60 * 1000;
-
-	// 	var d = Math.floor(left / a_day)
-	// 	var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-	// 	var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-	// 	var s = Math.floor((left % a_day) / 1000) % 60 % 60
-
-	// 	$("#day").text(d);
-	// 	$("#hour").text(h);
-	// 	$("#min").text(m);
-	// 	$("#sec").text(s);
-	// 	setTimeout('countDown()', 1000);
-	// }
+	var tv00before = new Date("2019/11/26 00:00");
+	if (now.getTime() > tv00before.getTime()) {
+		$('.t05').hide();
+		$('.t04').hide();
+		$('.t03').hide();
+		$('.t02').hide();
+		$('.t01').hide();
+	}
+	var tv05 = new Date("2019/11/26 00:00");
+	if (now.getTime() > tv05.getTime()) {
+		$('.t05').show();
+	}
+	var tv04 = new Date("2019/11/27 00:00");
+	if (now.getTime() > tv04.getTime()) {
+		$('.t05').hide();
+		$('.t04').show();
+	}
+	var tv03 = new Date("2019/11/28 00:00");
+	if (now.getTime() > tv03.getTime()) {
+		$('.t04').hide();
+		$('.t03').show();
+	}
+	var tv02 = new Date("2019/11/29 00:00");
+	if (now.getTime() > tv02.getTime()) {
+		$('.t03').hide();
+		$('.t02').show();
+	}
+	var tv01 = new Date("2019/11/30 00:00");
+	if (now.getTime() > tv01.getTime()) {
+		$('.t02').hide();
+		$('.t01').show();
+	}
 }
 
 
