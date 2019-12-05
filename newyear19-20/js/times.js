47143fc248492d363d9de34da21908d5d47b5e8f
kazua <tabuchi_kazuaki@abema.tv>
2019-12-05 11:11:57 +0900

    no message

--- newyear19-20/js/times.js
+++ newyear19-20/js/times.js
@@ -1,42 +1,25 @@
 $(document).ready(function() {
 	var now = new Date();
-	var def = new Date("2019/05/07 00:00");
+	var def = new Date("2019/12/01 00:00");
 	if (now.getTime() > def.getTime()) {
-		$('#bag07').hide();
-
+		$('#ta04').hide();
+		$('#ta07').hide();
 	}
-
-	var now = new Date();
-	var tt00 = new Date("2019/06/07 00:00");
-	if (now.getTime() > tt00.getTime()) {
-		$('#bag07').show();
-	}
-	var now = new Date();
-	var tl00 = new Date("2019/06/08 00:00");
-	if (now.getTime() > tl00.getTime()) {
-		$('#bag07').hide();
-	}
-
-	var now = new Date();
-	var tt01 = new Date("2019/06/14 00:00");
+	var tt01 = new Date("2019/12/12 23:59");
 	if (now.getTime() > tt01.getTime()) {
-		$('#bag07').hide();
+		$('#ta01').hide();
 	}
-	var now = new Date();
-	var tl01 = new Date("2019/06/15 00:00");
-	if (now.getTime() > tl01.getTime()) {
-		$('#bag07').hide();
-	}
-
-	var now = new Date();
-	var tt02 = new Date("2019/06/21 00:00");
+	var tt02 = new Date("2019/12/13 00:00");
 	if (now.getTime() > tt02.getTime()) {
-		$('#bag07').hide();
+		$('#ta04').show();
 	}
-	var now = new Date();
-	var tl02 = new Date("2019/06/22 00:00");
-	if (now.getTime() > tl02.getTime()) {
-		$('#bag07').hide();
+	var tl03 = new Date("2019/12/20 00:00");
+	if (now.getTime() > tl03.getTime()) {
+		$('#ta04').hide();
+		$('#ta07').show();
 	}
-
+	var tl04 = new Date("2019/12/26");
+	if (now.getTime() > tl04.getTime()) {
+		$('#ta07').hide();
+	}
 });
