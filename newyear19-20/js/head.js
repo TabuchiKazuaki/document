8168f470fc9daa24d2eac36ba4db648e38da2f75
kazua <tabuchi_kazuaki@abema.tv>
2019-12-11 14:13:07 +0900

    no message

--- newyear19-20/js/head.js
+++ newyear19-20/js/head.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function() {
 	var now = new Date();
-	var def = new Date("2019/01/01 00:00");
+	var def = new Date("2020/01/01 00:00");
 	if (now.getTime() > def.getTime()) {
 		$('.h02').show();
 		$('.h01').hide();
