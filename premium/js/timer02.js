d04d9286f9f6e59c317f55e6f01e58627c4e2190
kazua <tabuchi_kazuaki@abema.tv>
2020-01-24 13:33:12 +0900

    no message

--- premium/js/timer02.js
+++ premium/js/timer02.js
@@ -4,23 +4,20 @@ $(function() {
 
 function countDown() {
 	var now = new Date();
-	var ts01 = new Date("2020/01/20");
-	if (now.getTime() > ts01.getTime()) {
-		var time = new Date();
-		var end = new Date("2020/01/30 23:59");
-		var left = end - time;
-		var a_day = 24 * 60 * 60 * 1000;
-		var d = Math.floor(left / a_day)
-		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-		var s = Math.floor((left % a_day) / 1000) % 60 % 60
+	var time = new Date();
+	var end = new Date("2020/02/10 23:59");
+	var left = end - time;
+	var a_day = 24 * 60 * 60 * 1000;
+	var d = Math.floor(left / a_day)
+	var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+	var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+	var s = Math.floor((left % a_day) / 1000) % 60 % 60
 
-		$("#day").text(d);
-		$("#hour").text(h);
-		$("#min").text(m);
-		$("#sec").text(s);
-		setTimeout('countDown()', 1000);
-	}
+	$("#day").text(d);
+	$("#hour").text(h);
+	$("#min").text(m);
+	$("#sec").text(s);
+	setTimeout('countDown()', 1000);
 }
 
 
