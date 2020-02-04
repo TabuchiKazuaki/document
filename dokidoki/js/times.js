54dd2e7398cdb9b82b1cb99e532cd8fbeb5913ba
kazua <tabuchi_kazuaki@abema.tv>
2020-02-04 18:13:18 +0900

    no message

--- dokidoki/js/times.js
+++ dokidoki/js/times.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var now = new Date();
 	var t01 = new Date("2020/02/01 00:00");
 	if (now.getTime() > t01.getTime()) {
-		$('#t01').text('テスト');
+		$('#t01').text('時限後の表示 点滅なし');
 		$('#t01').removeClass('animated')
 	}
 });
