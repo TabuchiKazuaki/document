2cc9c22137235b14df558be70cb59b7bcb0c9cca
kazua <tabuchi_kazuaki@abema.tv>
2020-04-08 12:23:23 +0900

    no message

--- 4th/js/ff.js
+++ 4th/js/ff.js
@@ -43,6 +43,7 @@ $(document).ready(function() {
 	}
 	var time04af = new Date("2020/04/17 00:00");
 	if (now.getTime() > time04af.getTime()) {
+		$('.t04').hide();
 	}
 
 	var time05 = new Date("2020/04/19 00:00");
