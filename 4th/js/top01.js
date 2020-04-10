e513c1b79fadb9cc2824067e6b18bcb53975105b
kazua <tabuchi_kazuaki@abema.tv>
2020-04-10 15:04:04 +0900

    no message

--- 4th/js/top01.js
+++ 4th/js/top01.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	if (now.getTime() > time01.getTime()) {
 		$('.t01').show();
 	}
-	var time01af = new Date("2020/04/15 23:59:59");
+	var time01af = new Date("2020/04/14 23:59:59");
 	if (now.getTime() > time01af.getTime()) {
 		$('.t01').hide();
 	}
@@ -24,7 +24,7 @@ $(document).ready(function() {
 	if (now.getTime() > time02.getTime()) {
 		$('.t02').show();
 	}
-	var time02af = new Date("2020/04/16 23:59:59");
+	var time02af = new Date("2020/04/15 23:59:59");
 	if (now.getTime() > time02af.getTime()) {
 		$('.t02').hide();
 	}
@@ -33,7 +33,7 @@ $(document).ready(function() {
 	if (now.getTime() > time03.getTime()) {
 		$('.t03').show();
 	}
-	var time03af = new Date("2020/04/17 23:59:59");
+	var time03af = new Date("2020/04/16 23:59:59");
 	if (now.getTime() > time03af.getTime()) {
 		$('.t03').hide();
 	}
@@ -42,7 +42,7 @@ $(document).ready(function() {
 	if (now.getTime() > time04.getTime()) {
 		$('.t04').show();
 	}
-	var time04af = new Date("2020/04/18 23:59:59");
+	var time04af = new Date("2020/04/17 23:59:59");
 	if (now.getTime() > time04af.getTime()) {
 		$('.t04').hide();
 	}
@@ -51,7 +51,7 @@ $(document).ready(function() {
 	if (now.getTime() > time05.getTime()) {
 		$('.t05').show();
 	}
-	var time05af = new Date("2020/04/19 23:59:59");
+	var time05af = new Date("2020/04/18 23:59:59");
 	if (now.getTime() > time05af.getTime()) {
 		$('.t06').hide();
 	}
@@ -60,7 +60,7 @@ $(document).ready(function() {
 	if (now.getTime() > time06.getTime()) {
 		$('.t06').show();
 	}
-	var time06af = new Date("2020/04/20 23:59:59");
+	var time06af = new Date("2020/04/19 23:59:59");
 	if (now.getTime() > time06af.getTime()) {
 		$('.t06').hide();
 	}
