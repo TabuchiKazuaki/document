6766d510366b322fa4af4864a8268ce2171e5ec8
kazua <tabuchi_kazuaki@abema.tv>
2020-04-10 12:21:29 +0900

    no message

--- 4th/js/top01.js
+++ 4th/js/top01.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
 	var now = new Date();
 
-	var def = new Date("2019/05/07 00:00");
-	if (now.getTime() > def.getTime()) {
-		$('.t01').hide();
-		$('.t02').hide();
-		$('.t03').hide();
-		$('.t04').hide();
-		$('.t05').hide();
-		$('.t06').hide();
-		$('.t07').hide();
-		$('.t08').hide();
-		$('.t09').hide();
-	}
-
 	var time01 = new Date("2020/04/11 00:00");
 	if (now.getTime() > time01.getTime()) {
 		// $('.t01').attr("href","https://abema.tv/video/episode/90-1182_s1_p5");
