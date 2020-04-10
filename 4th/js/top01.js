72f2ef567a1fd821e49474f1fb0ee3bade301e04
kazua <tabuchi_kazuaki@abema.tv>
2020-04-10 14:59:42 +0900

    no message

--- 4th/js/top01.js
+++ 4th/js/top01.js
@@ -1,57 +1,68 @@
 $(document).ready(function() {
 	var now = new Date();
+	var def = new Date("2018/04/11 00:00");
+	if (now.getTime() > def.getTime()) {
+		$('.t01').hide();
+		$('.t02').hide();
+		$('.t03').hide();
+		$('.t04').hide();
+		$('.t05').hide();
+		$('.t06').hide();
+	}
+
 
 	var time01 = new Date("2020/04/11 00:00");
 	if (now.getTime() > time01.getTime()) {
-		// $('.t01').attr("href","https://abema.tv/video/episode/90-1182_s1_p5");
+		$('.t01').show();
 	}
-	var time01af = new Date("2020/04/15 00:00");
+	var time01af = new Date("2020/04/15 23:59:59");
 	if (now.getTime() > time01af.getTime()) {
+		$('.t01').hide();
 	}
 
 	var time02 = new Date("2020/04/15 00:00");
 	if (now.getTime() > time02.getTime()) {
-		$('.t02').addClass('.al_ad');
+		$('.t02').show();
 	}
-	var time02af = new Date("2020/04/16 00:00");
+	var time02af = new Date("2020/04/16 23:59:59");
 	if (now.getTime() > time02af.getTime()) {
-		$('.t02').removeClass('.al_ad');
+		$('.t02').hide();
 	}
 
 	var time03 = new Date("2020/04/16 00:00");
 	if (now.getTime() > time03.getTime()) {
-		$('.t03').addClass('.al_ad');
+		$('.t03').show();
 	}
-	var time03af = new Date("2020/04/17 00:00");
+	var time03af = new Date("2020/04/17 23:59:59");
 	if (now.getTime() > time03af.getTime()) {
-		$('.t03').removeClass('.al_ad');
+		$('.t03').hide();
 	}
 
 	var time04 = new Date("2020/04/17 00:00");
 	if (now.getTime() > time04.getTime()) {
-		$('.t04').addClass('.al_ad');
+		$('.t04').show();
 	}
-	var time04af = new Date("2020/04/18 00:00");
+	var time04af = new Date("2020/04/18 23:59:59");
 	if (now.getTime() > time04af.getTime()) {
-		$('.t04').removeClass('.al_ad');
+		$('.t04').hide();
 	}
 
 	var time05 = new Date("2020/04/18 00:00");
 	if (now.getTime() > time05.getTime()) {
-		$('.t05').addClass('.al_ad');
+		$('.t05').show();
 	}
-	var time05af = new Date("2020/04/19 00:00");
+	var time05af = new Date("2020/04/19 23:59:59");
 	if (now.getTime() > time05af.getTime()) {
-		$('.t05').removeClass('.al_ad');
+		$('.t06').hide();
 	}
 
 	var time06 = new Date("2020/04/19 00:00");
 	if (now.getTime() > time06.getTime()) {
-		$('.t06').addClass('.al_ad');
+		$('.t06').show();
 	}
-	var time06af = new Date("2020/04/20 00:00");
+	var time06af = new Date("2020/04/20 23:59:59");
 	if (now.getTime() > time06af.getTime()) {
-		$('.t06').removeClass('.al_ad');
+		$('.t06').hide();
 	}
 
 
