241c6fd7dab60d072a4354536f65c46d2cf22e6f
kazua <tabuchi_kazuaki@abema.tv>
2020-04-13 11:06:42 +0900

    no message

--- 4th/js/men.js
+++ 4th/js/men.js
@@ -15,92 +15,92 @@ $(document).ready(function() {
 		$('.t10').hide();
 	}
 
-	var time01 = new Date("2020/04/11 0:00");
+	var time01 = new Date("2020/04/11 00:00");
 	if (now.getTime() > time01.getTime()) {
 		$('.t01').show();
 	}
-	var time01af = new Date("2020/04/12 0:00");
+	var time01af = new Date("2020/04/12 00:00");
 	if (now.getTime() > time01af.getTime()) {
 		$('.t01').hide();
 	}
 
-	var time02 = new Date("2020/04/19 0:00");
+	var time02 = new Date("2020/04/19 00:00");
 	if (now.getTime() > time02.getTime()) {
 		$('.t02').show();
 	}
-	var time02af = new Date("2020/04/20 0:00");
+	var time02af = new Date("2020/04/20 00:00");
 	if (now.getTime() > time02af.getTime()) {
 		$('.t02').hide();
 	}
 
-	var time03 = new Date("2020/04/12 0:00");
+	var time03 = new Date("2020/04/12 00:00");
 	if (now.getTime() > time03.getTime()) {
 		$('.t03').show();
 	}
-	var time03af = new Date("2020/04/13 0:00");
+	var time03af = new Date("2020/04/13 00:00");
 	if (now.getTime() > time03af.getTime()) {
 		$('.t03').hide();
 	}
 
-	var time04 = new Date("2020/04/11 0:00");
+	var time04 = new Date("2020/04/11 00:00");
 	if (now.getTime() > time04.getTime()) {
 		$('.t04').show();
 	}
-	var time04af = new Date("2020/04/12 0:00");
+	var time04af = new Date("2020/04/12 00:00");
 	if (now.getTime() > time04af.getTime()) {
 		$('.t04').hide();
 	}
 
-	var time05 = new Date("2020/04/16 0:00");
+	var time05 = new Date("2020/04/16 00:00");
 	if (now.getTime() > time05.getTime()) {
 		$('.t05').show();
 	}
-	var time05af = new Date("2020/04/17 0:00");
+	var time05af = new Date("2020/04/17 00:00");
 	if (now.getTime() > time05af.getTime()) {
 		$('.t05').hide();
 	}
 
-	var time06 = new Date("2020/04/15 0:00");
+	var time06 = new Date("2020/04/15 00:00");
 	if (now.getTime() > time06.getTime()) {
 		$('.t06').show();
 	}
-	var time06af = new Date("2020/04/16 0:00");
+	var time06af = new Date("2020/04/16 00:00");
 	if (now.getTime() > time06af.getTime()) {
 		$('.t06').hide();
 	}
 
-	var time07 = new Date("2020/04/18 0:00");
+	var time07 = new Date("2020/04/18 00:00");
 	if (now.getTime() > time07.getTime()) {
 		$('.t07').show();
 	}
-	var time07af = new Date("2020/04/19 0:00");
+	var time07af = new Date("2020/04/19 00:00");
 	if (now.getTime() > time07af.getTime()) {
 		$('.t07').hide();
 	}
 
-	var time08 = new Date("2020/04/13 0:00");
+	var time08 = new Date("2020/04/13 00:00");
 	if (now.getTime() > time08.getTime()) {
 		$('.t08').show();
 	}
-	var time08af = new Date("2020/04/14 0:00");
+	var time08af = new Date("2020/04/14 00:00");
 	if (now.getTime() > time08af.getTime()) {
 		$('.t08').hide();
 	}
 
-	var time09 = new Date("2020/04/17 0:00");
+	var time09 = new Date("2020/04/17 00:00");
 	if (now.getTime() > time09.getTime()) {
 		$('.t09').show();
 	}
-	var time09af = new Date("2020/04/18 0:00");
+	var time09af = new Date("2020/04/18 00:00");
 	if (now.getTime() > time09af.getTime()) {
 		$('.t09').hide();
 	}
 
-	var time10 = new Date("2020/04/14 0:00");
+	var time10 = new Date("2020/04/14 00:00");
 	if (now.getTime() > time10.getTime()) {
 		$('.t10').show();
 	}
-	var time10af = new Date("2020/04/15 0:00");
+	var time10af = new Date("2020/04/15 00:00");
 	if (now.getTime() > time10af.getTime()) {
 		$('.t10').hide();
 	}
