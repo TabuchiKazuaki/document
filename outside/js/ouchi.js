85978db558e2d8449a0bd4fd34bc83ed7eb4adc4
kazua <tabuchi_kazuaki@abema.tv>
2020-04-28 12:22:36 +0900

    no message

---
+++ outside/js/ouchi.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+	var now = new Date();
+	var t01 = new Date("2020/05/04 00:00");
+	if (now.getTime() > t01.getTime()) {
+		$('.a01').attr("href","https://abema.tv/video/title/90-1383");
+		$('.a01').text("今すぐみる");
+	}
+
+	var t02 = new Date("2020/05/02 22:45");
+	if (now.getTime() > t02.getTime()) {
+		$('.a02').attr("href","https://abema.tv/video/episode/256-53_s1_p1");
+		$('.a02').text("今すぐみる");
+	}
+
+	var t03 = new Date("2020/05/03 22:00");
+	if (now.getTime() > t03.getTime()) {
+		$('.a03').attr("href","https://abema.tv/video/episode/90-493_s0_p150");
+		$('.a03').text("今すぐみる");
+	}
+
+	var t04 = new Date("2020/05/04 22:30");
+	if (now.getTime() > t04.getTime()) {
+		$('.a04').attr("href","https://abema.tv/video/episode/256-53_s1_p2");
+		$('.a04').text("今すぐみる");
+	}
+
+	var t05 = new Date("2020/05/06 18:00");
+	if (now.getTime() > t05.getTime()) {
+		$('.a05').attr("href","https://abema.tv/video/episode/90-1372_s1_p6");
+		$('.a05').text("今すぐみる");
+	}
+
+	var t06 = new Date("2020/05/07 00:15");
+	if (now.getTime() > t06.getTime()) {
+		$('.a06').attr("href","https://abema.tv/video/episode/256-53_s1_p3");
+		$('.a06').text("今すぐみる");
+	}
+
+	var t07 = new Date("2020/05/03 13:00");
+	if (now.getTime() > t07.getTime()) {
+		$('.a07').attr("href","https://abema.tv/video/title/524-2");
+		$('.a07').text("今すぐみる");
+	}
+
+	var t08 = new Date("2020/05/06 00:00");
+	if (now.getTime() > t08.getTime()) {
+		$('.a08').attr("href","https://abema.tv/video/title/90-1384");
+		$('.a08').text("今すぐみる");
+	}
+
+	var t09 = new Date("2020/05/07 00:00");
+	if (now.getTime() > t09.getTime()) {
+		$('.a09').attr("href","https://abema.tv/video/title/115-71");
+		$('.a09').text("今すぐみる");
+	}
+
+	var t10 = new Date("2020/05/10 22:40");
+	if (now.getTime() > t10.getTime()) {
+		$('.a10').attr("href","https://abema.tv/video/episode/12-18_s0_p9");
+		$('.a10').text("今すぐみる");
+	}
+
+	var t11 = new Date("2020/05/04 00:00");
+	if (now.getTime() > t11.getTime()) {
+		$('.a11').attr("href","https://abema.tv/video/title/13-116");
+		$('.a11').text("今すぐみる");
+	}
+
+	var t12 = new Date("2020/05/04 18:30");
+	if (now.getTime() > t12.getTime()) {
+		$('.a12').attr("href","https://abema.tv/video/title/274-17");
+	}
+
+	var t13 = new Date("2020/04/29 18:40");
+	if (now.getTime() > t13.getTime()) {
+		$('.a13').attr("href","https://abema.tv/video/title/161-15");
+	}
+
+	var t14 = new Date("2020/05/05 18:30");
+	if (now.getTime() > t14.getTime()) {
+		$('.a14').attr("href","https://abema.tv/video/title/274-15");
+	}
+
+});
