23523b1952abdc8a9b2eb11ebd28fde69f21fa30
kazua <tabuchi_kazuaki@abema.tv>
2020-06-02 16:43:40 +0900

    no message

---
+++ new_re/js/time02.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+	var now = new Date();
+	var tt05 = new Date("2020/06/13 00:00");
+	if (now.getTime() > tt05.getTime()) {
+		$('#t05').attr("href","https://abema.tv/video/episode/26-96_s2_p10");
+		$('#t05').text("ビデオでみる");
+	}
+
+
+	var tt06 = new Date("2020/06/21 17:00");
+	if (now.getTime() > tt06.getTime()) {
+		$('#t06').attr("href","https://abema.tv/video/title/526-1");
+		$('#t06').text("ビデオでみる");
+		$('#t07').attr("href","https://abema.tv/video/title/526-1");
+		$('#t07').text("ビデオでみる");
+		$('#t08').attr("href","https://abema.tv/video/title/526-1");
+		$('#t08').text("ビデオでみる");
+	}
+	var tt09 = new Date("2020/06/20 00:00");
+	if (now.getTime() > tt09.getTime()) {
+		$('#t09').attr("href","https://abema.tv/video/episode/26-96_s2_p11");
+		$('#t09').text("ビデオでみる");
+	}
+	var tt10 = new Date("2020/06/17 08:00");
+	if (now.getTime() > tt10.getTime()) {
+		$('#t10').attr("href","https://abema.tv/video/title/444-4");
+		$('#t10').text("ビデオでみる");
+	}
+
+	var tt11 = new Date("2020/06/18 08:00");
+	if (now.getTime() > tt11.getTime()) {
+		$('#t11').attr("href","https://abema.tv/video/title/444-4");
+		$('#t11').text("ビデオでみる");
+	}
+	var tt12 = new Date("2020/06/21 08:00");
+	if (now.getTime() > tt12.getTime()) {
+		$('#t12').attr("href","https://abema.tv/video/title/444-4");
+		$('#t12').text("ビデオでみる");
+	}
+	var tt13 = new Date("2020/06/21 08:00");
+	if (now.getTime() > tt13.getTime()) {
+		$('#t13').attr("href","https://abema.tv/video/title/444-4");
+		$('#t13').text("ビデオでみる");
+	}
+
+
+	var tt14 = new Date("2020/06/27 00:00");
+	if (now.getTime() > tt14.getTime()) {
+		$('#t14').attr("href","https://abema.tv/video/episode/26-96_s2_p12");
+		$('#t14').text("ビデオでみる");
+	}
+	var tt15 = new Date("2020/06/25 08:00");
+	if (now.getTime() > tt15.getTime()) {
+		$('#t15').attr("href","https://abema.tv/video/title/444-4");
+		$('#t15').text("ビデオでみる");
+	}
+	var tt16 = new Date("2020/06/25 08:00");
+	if (now.getTime() > tt16.getTime()) {
+		$('#t16').attr("href","https://abema.tv/video/title/444-4");
+		$('#t16').text("ビデオでみる");
+	}
+});
+
