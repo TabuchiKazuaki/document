661bdb0f8a5c52e7b8105e7d7f60c7e532be56f6
kazua <tabuchi_kazuaki@abema.tv>
2020-06-25 13:34:26 +0900

    no message

---
+++ outside/js/june_timer.js
@@ -0,0 +1,20 @@
+$(function() {
+     countDown();
+});
+function countDown() {
+     var now = new Date();
+     var time = new Date();
+     var end = new Date("2020/06/30 23:56:59");
+     var left = end - time;
+     var a_day = 24 * 60 * 60 * 1000;
+     var d = Math.floor(left / a_day)
+     var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+     var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+     var s = Math.floor((left % a_day) / 1000) % 60 % 60
+
+     $("#day").text(d);
+     $("#hour").text(h);
+     $("#min").text(m);
+     $("#sec").text(s);
+     setTimeout('countDown()', 1000);
+}
