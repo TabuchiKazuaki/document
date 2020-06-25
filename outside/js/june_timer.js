45a297aba4f77c009348844d4bff6e9b1da25e7f
kazua <tabuchi_kazuaki@abema.tv>
2020-06-25 20:06:48 +0900

    no message

--- outside/js/june_timer.js
+++ outside/js/june_timer.js
@@ -18,3 +18,13 @@ function countDown() {
      $("#sec").text(s);
      setTimeout('countDown()', 1000);
 }
+
+
+
+$(document).ready(function() {
+     var now = new Date();
+     var t01 = new Date("2020/07/01 00:00");
+     if (now.getTime() > t01.getTime()) {
+          $('.t_box').hide();
+     }
+});
