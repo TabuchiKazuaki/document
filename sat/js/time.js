6325c48e0782a786ed150f46f7c3eb0a325dc911
kazua <tabuchi_kazuaki@abema.tv>
2020-08-03 13:26:50 +0900

    no message

--- sat/js/time.js
+++ sat/js/time.js
@@ -4,24 +4,7 @@ $(function() {
 
 function countDown() {
 	var now = new Date();
-	// var ts00 = new Date("2019/11/02");
-	// if (now.getTime() > ts00.getTime()) {
-	// 	var time = new Date();
-	// 	var end = new Date("2019/11/04 23:59");
-	// 	var left = end - time;
-	// 	var a_day = 24 * 60 * 60 * 1000;
 
-	// 	var d = Math.floor(left / a_day)
-	// 	var h = Math.floor((left % a_day) / (60 * 60 * 1000))
-	// 	var m = Math.floor((left % a_day) / (60 * 1000)) % 60
-	// 	var s = Math.floor((left % a_day) / 1000) % 60 % 60
-
-	// 	$("#day").text(d);
-	// 	$("#hour").text(h);
-	// 	$("#min").text(m);
-	// 	$("#sec").text(s);
-	// 	setTimeout('countDown()', 1000);
-	// }
 	var ts01 = new Date("2019/11/09");
 	if (now.getTime() > ts01.getTime()) {
 		var time = new Date();
