2f7ce218004cfdd76177c0980a73e90a7494a290
kazua <tabuchi_kazuaki@abema.tv>
2020-08-17 12:11:25 +0900

    no message

--- sat/js/time.js
+++ sat/js/time.js
@@ -8,7 +8,7 @@ function countDown() {
 	var ts01 = new Date("2020/07/09 23:59");
 	if (now.getTime() > ts01.getTime()) {
 		var time = new Date();
-		var end = new Date("2020/08/30 23:59");
+		var end = new Date("2020/08/29 00:00");
 		var left = end - time;
 		var a_day = 24 * 60 * 60 * 1000;
 		var d = Math.floor(left / a_day)
@@ -22,13 +22,22 @@ function countDown() {
 		$("#sec").text(s);
 		setTimeout('countDown()', 1000);
 	}
-	var tv01 = new Date("2019/01/30 00:00");
-	if (now.getTime() > tv01.getTime()) {
-		$('.t02').hide();
-		$('.t01').show();
+	var bef = new Date("2020/08/01 00:00");
+	if (now.getTime() > bef.getTime()) {
+		$('.h_j').show();
 	}
 
+	var bef = new Date("2020/08/01 00:00");
+	if (now.getTime() > bef.getTime()) {
+		$('.h_j').show();
+	}
+	var before = new Date("2020/08/29 00:00");
+	if (now.getTime() > before.getTime()) {
+		$('.h_j').hide();
+		$('.h_be').show();
+	}
 }
+
 
 
 
