d366f558821672bbcd802d4e15451ed5feddc350
kazua <tabuchi_kazuaki@abema.tv>
2020-12-10 12:37:27 +0900

    no message

---
+++ premium/js/timer18.js
@@ -0,0 +1,60 @@
+
+$(function() {
+	countDown();
+});
+
+function countDown() {
+	var now = new Date();
+
+	var ts01 = new Date("2020/10/08 23:59");
+	if (now.getTime() > ts01.getTime()) {
+		var time = new Date();
+		var end = new Date("2020/12/20 23:59");
+		var left = end - time;
+		var a_day = 24 * 60 * 60 * 1000;
+		var d = Math.floor(left / a_day)
+		var h = Math.floor((left % a_day) / (60 * 60 * 1000))
+		var m = Math.floor((left % a_day) / (60 * 1000)) % 60
+		var s = Math.floor((left % a_day) / 1000) % 60 % 60
+
+		$("#day").text(d);
+		$("#hour").text(h);
+		$("#min").text(m);
+		$("#sec").text(s);
+		setTimeout('countDown()', 1000);
+	}
+
+	var bef = new Date("2020/08/02 00:00");
+	if (now.getTime() > bef.getTime()) {
+		$('.t_box').show();
+		$('.timer').show();
+	}
+	var bef02 = new Date("2020/12/19 23:59");
+	if (now.getTime() > bef02.getTime()) {
+		$("#day").hide();
+		$(".tdd").hide();
+	}
+	var aft = new Date("2020/12/20 23:59");
+	if (now.getTime() > aft.getTime()) {
+		$('.timer').hide();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
