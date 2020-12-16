ce9ae17552da6eb81172fe8514b23993152267a2
kazua <tabuchi_kazuaki@abema.tv>
2020-12-16 11:43:40 +0900

    no message

---
+++ newyear21/js/time_pre01.js
@@ -0,0 +1,93 @@
+
+$(function() {
+	countDown();
+});
+
+function countDown() {
+	var now = new Date();
+	var bef02 = new Date("2020/10/01 23:59");
+	if (now.getTime() > bef02.getTime()) {
+		$(".h_be").show();
+		$(".h_a").hide();
+	}
+	var aft = new Date("2021/01/01 00:00");
+	if (now.getTime() > aft.getTime()) {
+		$(".h_be").hide();
+		$(".h_a").show();
+	}
+
+
+	var time01 = new Date("2020/12/26 21:00");
+	if (now.getTime() > time01.getTime()) {
+		$(".t01").text("さっそくみる");
+		$(".t01").attr("href","abema");
+	}
+	var time01_af = new Date("2021/01/02 23:00");
+	if (now.getTime() > time01_af.getTime()) {
+		$(".t01").text("さっそくみる");
+		$(".t01").attr("href","abema");
+	}
+
+	var time02 = new Date("2020/12/26 21:00");
+	if (now.getTime() > time02.getTime()) {
+		$(".t02").text("さっそくみる");
+		$(".t02").attr("href","abema");
+	}
+	var time02_af = new Date("2021/01/03 22:00");
+	if (now.getTime() > time02_af.getTime()) {
+		$(".t02").text("さっそくみる");
+		$(".t02").attr("href","abema");
+	}
+
+	var time03 = new Date("2020/12/29 22:00");
+	if (now.getTime() > time03.getTime()) {
+		$(".t03").text("さっそくみる");
+		$(".t03").attr("href","abema");
+	}
+	var time03_af = new Date("2021/01/04 22:00");
+	if (now.getTime() > time03_af.getTime()) {
+		$(".t03").text("さっそくみる");
+		$(".t03").attr("href","abema");
+	}
+
+	var time04 = new Date("2020/12/31 18:00");
+	if (now.getTime() > time04.getTime()) {
+		$(".t04").text("さっそくみる");
+		$(".t04").attr("href","abema");
+	}
+	var time04_af = new Date("2021/01/05 18:00");
+	if (now.getTime() > time04_af.getTime()) {
+		$(".t04").text("さっそくみる");
+		$(".t04").attr("href","abema");
+	}
+
+	var time05 = new Date("2020/12/29 20:00");
+	if (now.getTime() > time05.getTime()) {
+		$(".t05").text("さっそくみる");
+		$(".t05").attr("href","abema");
+	}
+	var time05_af = new Date("2021/01/05 20:00");
+	if (now.getTime() > time05_af.getTime()) {
+		$(".t05").text("さっそくみる");
+		$(".t05").attr("href","abema");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
