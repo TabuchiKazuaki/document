a81c3b6bc6a72c3930a72d39ebdaea06c2dfee13
kazua <tabuchi_kazuaki@abema.tv>
2020-12-17 12:04:06 +0900

    no message

--- newyear21/js/time_pre03.js
+++ newyear21/js/time_pre03.js
@@ -9,8 +9,10 @@ function countDown() {
 	if (now.getTime() > bef02.getTime()) {
 		$(".h_be").show();
 		$(".h_a").hide();
-		$(".bt01af").hide();
 		$(".bt01be").show();
+		$(".bt01af").hide();
+		$(".bt02be").show();
+		$(".bt02af").hide();
 	}
 	var aft = new Date("2021/01/01 00:00");
 	if (now.getTime() > aft.getTime()) {
@@ -19,49 +21,29 @@ function countDown() {
 	}
 
 
-	var time01 = new Date("2021/01/01 15:00");
+	var time01 = new Date("2021/01/05 00:00");
 	if (now.getTime() > time01.getTime()) {
-		$(".t01").text("さっそくみる");
-		$(".t01").attr("href","https://abema.tv/video/title/90-980");
-	}
-	var time01_af = new Date("2021/01/10 23:00");
-	if (now.getTime() > time01_af.getTime()) {
-		$(".t01").text("さっそくみる");
-		$(".t01").attr("href","https://abema.tv/video/title/90-980");
+		$(".bt01be").hide();
+		$(".bt01af").show();
 	}
 
-	var time02 = new Date("2021/01/01 06:00");
+	var time02 = new Date("2021/01/01 00:00");
 	if (now.getTime() > time02.getTime()) {
-		$(".t02").text("さっそくみる");
-		$(".t02").attr("href","abema");
+		$(".bt02be").hide();
+		$(".bt02af").show();
 	}
-	// var time02_af = new Date("2021/01/03 22:00");
-	// if (now.getTime() > time02_af.getTime()) {
-	// 	$(".t02").text("さっそくみる");
-	// 	$(".t02").attr("href","abema");
-	// }
 
 	var time03 = new Date("2021/01/02 22:00");
 	if (now.getTime() > time03.getTime()) {
 		$(".t03").text("さっそくみる");
 		$(".t03").attr("href","abema");
 	}
-	// var time03_af = new Date("2021/01/04 22:00");
-	// if (now.getTime() > time03_af.getTime()) {
-	// 	$(".t03").text("さっそくみる");
-	// 	$(".t03").attr("href","abema");
-	// }
 
-	var time04 = new Date("2021/01/02 22:00");
+	var time04 = new Date("2021/01/01 00:00");
 	if (now.getTime() > time04.getTime()) {
 		$(".t04").text("さっそくみる");
-		$(".t04").attr("href","abema");
+		$(".t04").attr("href","https://abema.tv/video/title/182-497");
 	}
-	// var time04_af = new Date("2021/01/05 18:00");
-	// if (now.getTime() > time04_af.getTime()) {
-	// 	$(".t04").text("さっそくみる");
-	// 	$(".t04").attr("href","abema");
-	// }
 
 	var time05 = new Date("2021/01/01 00:00");
 	if (now.getTime() > time05.getTime()) {
@@ -69,11 +51,6 @@ function countDown() {
 		$(".t05").attr("href","abema");
 		$(".bt01af").show();
 		$(".bt01be").hide();
-	}
-	var time05_af = new Date("2021/01/01 00:00");
-	if (now.getTime() > time05_af.getTime()) {
-		$(".t05").text("https://abema.tv/video/title/50-59");
-		$(".t05").attr("href","abema");
 	}
 }
 
